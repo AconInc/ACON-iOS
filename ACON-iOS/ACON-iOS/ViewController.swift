3e56b87c05e6bbe401ce3404aa3ea5155075d299
Sumin <86866423+cirtuare@users.noreply.github.com>
2025-01-06 14:09:40 +0900

    [Setting] 프로젝트 초기 세팅 (#1)

---
+++ ACON-iOS/ACON-iOS/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ACON-iOS
+//
+//  Created by 이수민 on 1/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .systemBlue
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
