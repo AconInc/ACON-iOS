6325da7c803894956617f6071a2438832a2217fa
cirtuare <cirtuare@gmail.com>
2025-01-06 17:41:16 +0900

    [Setting] 초기 폴더링 및 ObservablePattern & ScreenUtils 추가 (#3)

---
+++ ACON-iOS/ACON-iOS/Global/Utils/ScreenUtils.swift
@@ -0,0 +1,25 @@
+//
+//  StringUtils.swift
+//  ACON-iOS
+//
+//  Created by 이수민 on 1/6/25.
+//
+
+import UIKit
+
+struct ScreenUtils {
+    
+    // MARK: 디바이스 width
+    
+    static var width: CGFloat {
+        return UIScreen.main.bounds.width
+    }
+    
+    
+    // MARK: 디바이스 height
+    
+    static var height: CGFloat {
+        return UIScreen.main.bounds.height
+    }
+    
+}
