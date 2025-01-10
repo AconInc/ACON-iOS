d5698d8b0bf24e60b88942cea32cf33bf15fa98e
Yurim Kim <yurim830@gmail.com>
2025-01-11 02:31:12 +0900

    [Add] ACTabBarController 생성 (#6)

---
+++ ACON-iOS/ACON-iOS/Presentation/TabBar/ACTabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  TabBarController.swift
+//  ACON-iOS
+//
+//  Created by 김유림 on 1/11/25.
+//
+
+import UIKit
+
+class ACTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
