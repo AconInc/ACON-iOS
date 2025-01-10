55bdbe9b1b1de451b64e9269823c54ed094ee107
Yurim Kim <yurim830@gmail.com>
2025-01-11 06:57:15 +0900

    [Feat] 탭바 아이템 구조 설정 (#6)

--- ACON-iOS/ACON-iOS/Presentation/TabBar/ACTabBarItem.swift
+++ ACON-iOS/ACON-iOS/Presentation/TabBar/ACTabBarItem.swift
@@ -5,4 +5,48 @@
 //  Created by 김유림 on 1/11/25.
 //
 
-import Foundation
+import UIKit
+
+enum ACTabBarItem: CaseIterable {
+    
+    case place, upload, profile
+    
+    var viewController: UIViewController {
+        switch self {
+        case .place: return PlaceViewController()
+        case .upload: return ViewController()
+        case .profile: return ViewController()
+        }
+    }
+    
+    var itemTitle: String {
+        switch self {
+        case .place: return "장소"
+        case .upload: return "업로드"
+        case .profile: return "프로필"
+        }
+    }
+    
+    var normalItemImage: UIImage {
+        switch self {
+        case .place:
+            return .icSpotGla24
+        case .upload:
+            return .icUploadGla24
+        case .profile:
+            return .icMypageGla24
+        }
+    }
+    
+    var selectedItemImage: UIImage {
+        switch self {
+        case .place:
+            return .icSpotW24
+        case .upload:
+            return .icUploadW24
+        case .profile:
+            return .icMypageW24
+        }
+    }
+    
+}
