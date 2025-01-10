b281deb9d7810742587440a6dbfac8280e0841ea
Yurim Kim <yurim830@gmail.com>
2025-01-11 06:57:52 +0900

    [Feat] 시작 화면 ACTabBarController로 변경 (#6)

--- ACON-iOS/ACON-iOS/Application/SceneDelegate.swift
+++ ACON-iOS/ACON-iOS/Application/SceneDelegate.swift
@@ -15,9 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         self.window = UIWindow(windowScene: windowScene)
-        let navigationController = UINavigationController(rootViewController: ViewController())
-        navigationController.navigationBar.isHidden = true
-        self.window?.rootViewController = navigationController
+        self.window?.rootViewController = ACTabBarController()
         self.window?.makeKeyAndVisible()
     }
 
