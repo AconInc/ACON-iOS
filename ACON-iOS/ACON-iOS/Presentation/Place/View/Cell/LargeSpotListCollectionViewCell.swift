65036c68710caa7e445dd3b1e303e09850c31266
Yurim Kim <yurim830@gmail.com>
2025-01-12 14:07:11 +0900

    [Add] Cell 폴더, LargeSpotListCollectionViewCell (#11)

---
+++ ACON-iOS/ACON-iOS/Presentation/Place/View/Cell/LargeSpotListCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  LargeSpotListCollectionViewCell.swift
+//  ACON-iOS
+//
+//  Created by 김유림 on 1/12/25.
+//
+
+import UIKit
+
+class LargeSpotListCollectionViewCell: UICollectionViewCell {
+    
+}
