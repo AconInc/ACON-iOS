deb63a76adcff4176564bc8f12fc281b14de3831
Yurim Kim <yurim830@gmail.com>
2025-01-12 15:18:13 +0900

    [Feat] 컬렉션뷰 flowlayout 설정 (#11)

--- ACON-iOS/ACON-iOS/Presentation/Place/View/SpotListView.swift
+++ ACON-iOS/ACON-iOS/Presentation/Place/View/SpotListView.swift
@@ -9,4 +9,53 @@ import UIKit
 
 class SpotListView: BaseView {
     
+    // MARK: - UI Properties
+    
+    let collectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewFlowLayout()
+    )
+    
+    
+    // MARK: - LifeCycles
+    
+    override func setHierarchy() {
+        super.setHierarchy()
+        
+        self.addSubviews(collectionView)
+    }
+    
+    override func setLayout() {
+        super.setLayout()
+        
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    override func setStyle() {
+        self.backgroundColor = .gray9
+        
+        setCollectionView()
+    }
+}
+
+
+// MARK: - UI Settings
+
+extension SpotListView {
+    func setCollectionView() {
+        let flowLayout = UICollectionViewFlowLayout()
+        let width: CGFloat = ScreenUtils.width - 40
+        let height: CGFloat = 408
+        
+        flowLayout.itemSize = CGSize(width: width, height: height)
+        flowLayout.minimumLineSpacing = 12
+        flowLayout.scrollDirection = .vertical
+        
+        collectionView.do {
+            $0.setCollectionViewLayout(flowLayout, animated: true)
+        }
+    }
+    
 }
