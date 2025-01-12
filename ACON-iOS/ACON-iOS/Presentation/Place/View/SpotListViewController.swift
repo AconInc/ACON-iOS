51d7897a68fe2da9001735f7955540de9dbb8286
Yurim Kim <yurim830@gmail.com>
2025-01-12 15:18:55 +0900

    [Feat] 컬렉션뷰 기본 세팅(delegate, register) (#11)

--- ACON-iOS/ACON-iOS/Presentation/Place/View/SpotListViewController.swift
+++ ACON-iOS/ACON-iOS/Presentation/Place/View/SpotListViewController.swift
@@ -9,11 +9,78 @@ import UIKit
 
 class SpotListViewController: BaseViewController {
     
+    // MARK: - UI Properties
+    
+    private let spotListView = SpotListView()
+    
+    
+    // MARK: - Properties
+    
+//    private let spotiewModel =
+    
+    
+    // MARK: - LifeCycle
+    
+    override func loadView() {
+        view = spotListView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .acBlack
+        setCollectionView()
     }
     
+    override func setHierarchy() {}
+    
+    override func setLayout() {}
+    
+    override func setStyle() {
+        self.view.backgroundColor = .gray9
+        self.navigationController?.navigationBar.isHidden = true
+        self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+    }
+    
+}
+
+
+// MARK: - CollectionView Settings
+
+extension SpotListViewController {
+    
+    private func setCollectionView() {
+        setDelegate()
+        registerCells()
+    }
+    
+    private func setDelegate() {
+        spotListView.collectionView.dataSource = self
+        spotListView.collectionView.delegate = self
+    }
+    
+    private func registerCells() {
+        spotListView.collectionView.register(
+            LargeSpotListCollectionViewCell.self,
+            forCellWithReuseIdentifier: LargeSpotListCollectionViewCell.cellIdentifier
+        )
+    }
+    
+}
+
+extension SpotListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let item = collectionView.dequeueReusableCell(withReuseIdentifier: LargeSpotListCollectionViewCell.cellIdentifier, for: indexPath) as? LargeSpotListCollectionViewCell
+        else { return UICollectionViewCell() }
+        
+        return item
+    }
+    
+}
+
+extension SpotListViewController: UICollectionViewDelegate {
     
 }
