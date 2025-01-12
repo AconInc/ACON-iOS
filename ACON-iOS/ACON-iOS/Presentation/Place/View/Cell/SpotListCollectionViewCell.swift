324afc5742b98e3c4ba0e5aa4488b94e04057f2a
Yurim Kim <yurim830@gmail.com>
2025-01-12 19:25:40 +0900

    [Feat] SpotList cell UI setting (#11)

--- ACON-iOS/ACON-iOS/Presentation/Place/View/Cell/SpotListCollectionViewCell.swift
+++ ACON-iOS/ACON-iOS/Presentation/Place/View/Cell/SpotListCollectionViewCell.swift
@@ -13,25 +13,123 @@ class SpotListCollectionViewCell: BaseCollectionViewCell {
     
     private let bgImage = UIImageView()
     
-    private let matchPercentageLabel = UILabel()
-    private let matchPercentageView = UIView()
+    private let matchingRateView = UIView()
+    private let matchingRateLabel = UILabel()
     
     private let stackView = UIStackView()
-    private let categoryLabel = UILabel()
-    private let spotNameLabel = UILabel()
+    
+    private let titleStackView = UIStackView()
+    private let typeLabel = UILabel()
+    private let nameLabel = UILabel()
+    
     private let timeInfoStackView = UIStackView()
-    private let personImageView = UIImageView()
-    private let timeLabel = UILabel()
+    private let walkingIcon = UIImageView()
+    private let walkingTimeLabel = UILabel()
     
     
     // MARK: - Life Cycle
     
-    override func setHierarchy() {}
+    override func setHierarchy() {
+        super.setHierarchy()
+        
+        self.addSubviews(bgImage,
+                         matchingRateView,
+                         stackView)
+        
+        matchingRateView.addSubview(matchingRateLabel)
+        
+        stackView.addArrangedSubviews(titleStackView,
+                                      timeInfoStackView)
+        
+        titleStackView.addArrangedSubviews(typeLabel,
+                                           nameLabel)
+        
+        timeInfoStackView.addArrangedSubviews(walkingIcon,
+                                              walkingTimeLabel)
+    }
     
-    override func setLayout() {}
+    override func setLayout() {
+        super.setLayout()
+        
+        bgImage.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        matchingRateView.snp.makeConstraints {
+            $0.leading.top.equalToSuperview().offset(16)
+        }
+        
+        matchingRateLabel.snp.makeConstraints {
+            $0.horizontalEdges.equalTo(matchingRateView).inset(7.5)
+            $0.verticalEdges.equalTo(matchingRateView).inset(2)
+        }
+        
+        stackView.snp.makeConstraints {
+            $0.leading.bottom.equalToSuperview().inset(16)
+        }
+    }
     
     override func setStyle() {
-        self.backgroundColor = .red
+        backgroundColor = .clear
+        
+        bgImage.do {
+            $0.contentMode = .scaleAspectFill
+            $0.layer.cornerRadius = 6
+            $0.clipsToBounds = true
+        }
+        
+        matchingRateView.do {
+            $0.backgroundColor = .gray9
+            $0.layer.contents = 2
+        }
+        
+        matchingRateLabel.do {
+            $0.setText(.b4, .acWhite)
+        }
+        
+        stackView.do {
+            $0.axis = .vertical
+            $0.spacing = 4
+        }
+        
+        titleStackView.do {
+            $0.axis = .vertical
+            $0.spacing = 0
+        }
+        
+        timeInfoStackView.do {
+            $0.axis = .horizontal
+            $0.spacing = 0
+        }
+        
+        walkingIcon.do {
+            $0.image = .icWalking24
+            $0.contentMode = .scaleAspectFit
+        }
+    }
+    
+}
+
+
+// MARK: - Binding
+
+extension SpotListCollectionViewCell {
+    
+    func bind(spot: SpotModel) {
+        bgImage.image = spot.image
+        
+        matchingRateLabel.setLabel(text: "취향 일치율 \(String(spot.matchingRate))%",
+                                   style: .b4)
+        
+        typeLabel.setLabel(text: spot.type,
+                           style: .b4)
+        
+        nameLabel.setLabel(text: spot.name,
+                           style: .h7)
+        
+        walkingTimeLabel.setLabel(text: "\(String(spot.walkingTime))분",
+                                  style: .b4,
+                                  color: .gray3)
     }
     
 }
