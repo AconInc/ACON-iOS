8200da516207add5da7b48395428c196f09037dc
cirtuare <cirtuare@gmail.com>
2025-01-13 06:23:39 +0900

    [Feat] SpotUploadVC 구현 (#14)

--- ACON-iOS/ACON-iOS/Presentation/Upload/View/SpotUploadViewController.swift
+++ ACON-iOS/ACON-iOS/Presentation/Upload/View/SpotUploadViewController.swift
@@ -7,7 +7,69 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 class SpotUploadViewController: BaseNavViewController {
     
+    // MARK: - UI Properties
+    
+    private let spotUploadView = SpotUploadView()
+    
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setXButton()
+        self.setSecondTitleLabelStyle(title: StringLiterals.Upload.spotUpload)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(false)
+
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func setHierarchy() {
+        super.setHierarchy()
+        
+        self.contentView.addSubview(spotUploadView)
+    }
+    
+    override func setLayout() {
+        super.setLayout()
+
+        spotUploadView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    override func setStyle() {
+        self.spotUploadView.dropAcornButton.isEnabled = false
+    }
+    
+    func addTarget() {
+        spotUploadView.spotSearchButton.addTarget(self, action: #selector(spotSearchButtonTapped), for: .touchUpInside)
+        spotUploadView.dropAcornButton.addTarget(self, action: #selector(dropAcornButtonTapped), for: .touchUpInside)
+    }
+
+}
+
+    
+// MARK: - @objc functions
+
+extension SpotUploadViewController {
+
+    @objc
+    func spotSearchButtonTapped() {
+        // TODO: - push to spotsearchvc
+    }
+    
+    @objc
+    func dropAcornButtonTapped() {
+        // TODO: - push to dropAcornvc
+    }
     
 }
