3d4b8e581323bab42c2fcd995333906dc66c501d
Yurim Kim <yurim830@gmail.com>
2025-01-14 03:42:40 +0900

    [Add] Model, ViewModel 생성 (#16)

---
+++ ACON-iOS/ACON-iOS/Presentation/SpotListFilter/Model/SpotListFilterModel.swift
@@ -0,0 +1,12 @@
+//
+//  SpotListFilterModel.swift
+//  ACON-iOS
+//
+//  Created by 김유림 on 1/14/25.
+//
+
+import Foundation
+
+struct SpotListFilterModel {
+    
+}
---
+++ ACON-iOS/ACON-iOS/Presentation/SpotListFilter/ViewModel/SpotListFilterViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SpotListFilterViewModel.swift
+//  ACON-iOS
+//
+//  Created by 김유림 on 1/14/25.
+//
+
+import Foundation
+
+class SpotListFilterViewModel {
+    
+}
