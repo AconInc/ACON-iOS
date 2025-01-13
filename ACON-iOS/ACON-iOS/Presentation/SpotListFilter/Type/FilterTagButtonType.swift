4b8a00775e1d89efa815b1417b430398e7308eb8
Yurim Kim <yurim830@gmail.com>
2025-01-14 05:58:02 +0900

    [Feat] 속성 설정 (#16)

--- ACON-iOS/ACON-iOS/Presentation/SpotListFilter/Type/FilterTagButtonType.swift
+++ ACON-iOS/ACON-iOS/Presentation/SpotListFilter/Type/FilterTagButtonType.swift
@@ -9,13 +9,24 @@ import UIKit
 
 enum FilterTagButtonType {
     
-    case unselected, selected
+    case selected, unselected
     
     var bgColor: UIColor {
         switch self {
-        case .unselected: return .gray8
         case .selected: return .subOrg35
+        case .unselected: return .gray8
         }
+    }
+    
+    var strokeColor: UIColor {
+        switch self {
+        case .selected: return .org1
+        case .unselected: return .gray6
+        }
+    }
+    
+    var textColor: UIColor {
+        return .acWhite
     }
     
 }
