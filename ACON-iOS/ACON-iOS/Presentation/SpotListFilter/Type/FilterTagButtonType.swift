1c36d81cdffe0153c87819f4e2afe5b851cd2bb8
Yurim Kim <yurim830@gmail.com>
2025-01-14 05:54:04 +0900

    [Feat] 타입 설정 (#16)

--- ACON-iOS/ACON-iOS/Presentation/SpotListFilter/Type/FilterTagButtonType.swift
+++ ACON-iOS/ACON-iOS/Presentation/SpotListFilter/Type/FilterTagButtonType.swift
@@ -5,4 +5,18 @@
 //  Created by 김유림 on 1/14/25.
 //
 
-import Foundation
+import UIKit
+
+enum FilterTagButtonType {
+    
+    case unselected, selected
+    
+    var bgColor: UIColor {
+        switch self {
+        case .unselected: return .gray8
+        case .selected: return .subOrg35
+        }
+    }
+    
+}
+
