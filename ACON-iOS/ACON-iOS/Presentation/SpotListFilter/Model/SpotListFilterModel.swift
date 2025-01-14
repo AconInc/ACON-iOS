04033fe59050d6485fe77a37b1a03b663b867181
Yurim Kim <yurim830@gmail.com>
2025-01-14 17:53:56 +0900

    [Feat] firstLine, secondLine 구분 (#16)

--- ACON-iOS/ACON-iOS/Presentation/SpotListFilter/Model/SpotListFilterModel.swift
+++ ACON-iOS/ACON-iOS/Presentation/SpotListFilter/Model/SpotListFilterModel.swift
@@ -9,4 +9,50 @@ import Foundation
 
 struct SpotListFilterModel {
     
+    struct RestaurantFeature {
+        static let firstLine: [SpotType.RestaurantFeatureType] = [
+            .korean, .western, .chinese, .japanese, .snack
+        ]
+        
+        static let secondLine: [SpotType.RestaurantFeatureType] = [
+            .asian, .bar, .excludeFranchise
+        ]
+    }
+    
+    struct CafeFeature {
+        
+        var firstLine: [SpotType.CafeFeatureType] {
+            return [.large, .goodView, .dessert, .terace]
+        }
+        
+        var secondLine: [SpotType.CafeFeatureType] {
+            return [.excludeFranchise]
+        }
+        
+    }
+    
+    struct Companion {
+        
+        var firstLine: [SpotType.CompanionType] {
+            return [.family, .date, .friend, .alone, .group]
+        }
+        
+        var secondLine: [SpotType.CompanionType] {
+            return []
+        }
+        
+    }
+    
+    struct VisitPurpose {
+        
+        var firstLine: [SpotType.VisitPurposeType] {
+            return [.meeting, .study]
+        }
+        
+        var secondLine: [SpotType.VisitPurposeType] {
+            return []
+        }
+        
+    }
+    
 }
