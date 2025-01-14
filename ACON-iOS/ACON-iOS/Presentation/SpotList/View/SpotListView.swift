acf4793a3fd433682bc56d4dfe6046b121b74d64
Yurim Kim <yurim830@gmail.com>
2025-01-15 01:48:48 +0900

    [Chore] NOTE 추가 및 삼항연산자 적용 (#11)

--- ACON-iOS/ACON-iOS/Presentation/SpotList/View/SpotListView.swift
+++ ACON-iOS/ACON-iOS/Presentation/SpotList/View/SpotListView.swift
@@ -48,7 +48,7 @@ extension SpotListView {
     
     func setCollectionView() {
         let flowLayout = UICollectionViewFlowLayout()
-        // 참고: itemSize는 Controller에서 설정합니다. (collectionView의 height이 필요하기 때문)
+        // NOTE: itemSize는 Controller에서 설정합니다. (collectionView의 height이 필요하기 때문)
         flowLayout.minimumLineSpacing = SpotListItemSizeType.minimumLineSpacing.value
         flowLayout.scrollDirection = .vertical
         
--- ACON-iOS/ACON-iOS/Presentation/SpotList/View/SpotListViewController.swift
+++ ACON-iOS/ACON-iOS/Presentation/SpotList/View/SpotListViewController.swift
@@ -61,20 +61,19 @@ private extension SpotListViewController {
         }
         
         DispatchQueue.main.async {
-            // 데이터 리로드 전 애니메이션
+            // NOTE: 데이터 리로드 전 애니메이션
             UIView.animate(withDuration: 0.25, animations: {
                 self.spotListView.collectionView.alpha = 0.5 // 투명도 낮춤
             }) { _ in
-                // 데이터 리로드
+                // NOTE: 데이터 리로드
                 self.spotListView.collectionView.reloadData()
                 
-                // 데이터 리로드 후 애니메이션 (천천히 올라오는 애니메이션)
+                // NOTE: 데이터 리로드 후 애니메이션 (천천히 올라오는 애니메이션)
                 UIView.animate(withDuration: 1.0, delay: 0, options: .curveEaseOut) {
-                    // Set content offset with slow animation
                     self.spotListView.collectionView.setContentOffset(.zero, animated: true)
                     self.spotListView.collectionView.alpha = 1.0 // 투명도 복원
                 } completion: { _ in
-                    // 리프레시 종료
+                    // NOTE: 리프레시 종료
                     self.spotListView.collectionView.refreshControl?.endRefreshing()
                 }
             }
@@ -142,13 +141,10 @@ extension SpotListViewController: UICollectionViewDataSource {
         
         let spot = isFirstPage ? spotListViewModel.firstSpotList[indexPath.row] : spotListViewModel.secondSpotList[indexPath.row]
         
-        // 1번페이지 1번째 셀만 취향 일치율 배경색 어둡게
-        if isFirstPage,
-           indexPath.item == 0 {
-            item.bind(spot: spot, matchingRateBgColor: .dark)
-        } else {
-            item.bind(spot: spot, matchingRateBgColor: .light)
-        }
+        // NOTE: 1번페이지 1번째 셀만 취향 일치율 배경색 어둡게
+        let bgColor: MatchingRateBgColorType = isFirstPage && indexPath.item == 0 ? .dark : .light
+        
+        item.bind(spot: spot, matchingRateBgColor: bgColor)
         
         return item
     }
@@ -163,7 +159,7 @@ extension SpotListViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         sizeForItemAt indexPath: IndexPath) -> CGSize {
-        // 1번페이지 1번째 셀만 크게
+        // NOTE: 1번페이지 1번째 셀만 크게
         guard let isFirstPage = spotListViewModel.isFirstPage.value
         else { return .zero }
         
