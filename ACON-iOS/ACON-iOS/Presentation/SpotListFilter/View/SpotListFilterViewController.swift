b81227fdf1287116d22119e198db66f31eb70bc8
Yurim Kim <yurim830@gmail.com>
2025-01-14 18:00:24 +0900

    [Feat] 스택뷰 클리어, 버튼 추가 메소드 구현 (#16)

--- ACON-iOS/ACON-iOS/Presentation/SpotListFilter/View/SpotListFilterViewController.swift
+++ ACON-iOS/ACON-iOS/Presentation/SpotListFilter/View/SpotListFilterViewController.swift
@@ -7,23 +7,101 @@
 
 import UIKit
 
-class SpotListFilterViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class SpotListFilterViewController: BaseNavViewController {
+    
+    // MARK: - Properties
+    
+    let spotListFilterView = SpotListFilterView()
+    let viewModel = SpotListFilterViewModel()
+    
+    
+    // MARK: - LifeCycles
+    
+    override func loadView() {
+        view = spotListFilterView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bindViewModel()
     }
-    */
+    
+}
 
+
+// MARK: - Binding ViewModel
+
+private extension SpotListFilterViewController {
+    
+    func bindViewModel() {
+        viewModel.spotType.value = .restaurant
+        
+        viewModel.spotType.bind { [weak self] spotType in
+            guard let self = self,
+                  let spotType = spotType
+            else { return }
+            
+            updateView(spotType)
+            print("updateView!")
+        }
+    }
+    
+}
+
+
+// MARK: - UI Update
+
+private extension SpotListFilterViewController {
+    
+    // TODO: restaurant
+    
+    func updateView(_ spotType: SpotType) {
+        updateFeatureStack(spotType)
+        
+    }
+    
+    private func updateFeatureStack(_ spotType: SpotType) {
+        let featureStack = spotListFilterView.spotFeatureStackView
+        
+        // clear stack
+        featureStack.clearStackView()
+        
+        // add buttons
+        for feature in SpotListFilterModel.RestaurantFeature.firstLine {
+            let btn = FilterTagButton()
+            
+            btn.setAttributedTitle(text: feature.text, style: .b3)
+            btn.addTarget(self,
+                          action: #selector(didTapFilterTagButton),
+                          for: .touchUpInside)
+            
+            featureStack.addTagButton(to: .first,
+                                      button: btn)
+        }
+        
+        for feature in SpotListFilterModel.RestaurantFeature.secondLine {
+            let btn = FilterTagButton()
+            btn.setAttributedTitle(text: feature.text, style: .b3)
+            btn.addTarget(self,
+                          action: #selector(didTapFilterTagButton),
+                          for: .touchUpInside)
+            
+            featureStack.addTagButton(to: .second,
+                                      button: btn)
+        }
+    }
+    
+}
+
+
+// MARK: - @objc functions
+
+private extension SpotListFilterViewController {
+    
+    @objc
+    func didTapFilterTagButton(button: UIButton) {
+        button.isSelected.toggle()
+    }
+    
 }
