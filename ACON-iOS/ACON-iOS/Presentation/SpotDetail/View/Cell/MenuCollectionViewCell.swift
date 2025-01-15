ef02daa27695666ce825ddf728bafbbf723f4837
cirtuare <cirtuare@gmail.com>
2025-01-16 06:35:04 +0900

    [Feat] MenuDetailCell 구현 (#27)

---
+++ ACON-iOS/ACON-iOS/Presentation/SpotDetail/View/Cell/MenuCollectionViewCell.swift
@@ -0,0 +1,78 @@
+//
+//  MenuCollectionViewCell.swift
+//  ACON-iOS
+//
+//  Created by 이수민 on 1/16/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class MenuCollectionViewCell: BaseCollectionViewCell {
+
+    // MARK: - UI Properties
+    
+    private let menuImageView: UIImageView = UIImageView()
+    
+    private let menuNameLabel: UILabel = UILabel()
+    
+    private let menuPriceLabel: UILabel = UILabel()
+    
+    
+    // MARK: - Lifecycle
+    
+    override func setHierarchy() {
+        super.setHierarchy()
+        
+        self.addSubviews(menuImageView,
+                         menuNameLabel,
+                         menuPriceLabel)
+    }
+    
+    override func setLayout() {
+        super.setLayout()
+        
+        menuImageView.snp.makeConstraints {
+            $0.verticalEdges.equalToSuperview().inset(ScreenUtils.height*16/780)
+            $0.leading.equalToSuperview()
+        }
+        
+        menuNameLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(ScreenUtils.height*31/780)
+            $0.leading.equalToSuperview().inset(ScreenUtils.width*94/360)
+            $0.trailing.equalToSuperview().inset(ScreenUtils.width*4/360)
+        }
+        
+        menuPriceLabel.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(ScreenUtils.height*31/780)
+            $0.leading.equalToSuperview().inset(ScreenUtils.width*94/360)
+            $0.trailing.equalToSuperview().inset(ScreenUtils.width*4/360)
+        }
+    }
+    
+    override func setStyle() {
+        super.setStyle()
+        
+        self.backgroundColor = .clear
+        menuImageView.do {
+            $0.image = .imgStoreDetailMenulist
+            $0.contentMode = .scaleAspectFill
+        }
+    }
+    
+}
+
+extension MenuCollectionViewCell {
+    
+    func dataBind(_ menuInfoData: SpotMenuInfo, _ indexRow: Int) {
+        // TODO: imageView kf
+        menuNameLabel.setLabel(text: menuInfoData.name,
+                               style: .b2)
+        // TODO: 가격 쉼표 넣기
+        menuPriceLabel.setLabel(text: String(menuInfoData.price) + "원",
+                               style: .s1)
+    }
+    
+}
