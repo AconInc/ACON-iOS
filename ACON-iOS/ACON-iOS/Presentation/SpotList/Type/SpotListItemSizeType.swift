7a0a9061441ed023bd4e862bf59c376f23314452
Yurim Kim <yurim830@gmail.com>
2025-01-15 22:42:07 +0900

    [Feat] 헤더 높이 추가 (#23)

--- ACON-iOS/ACON-iOS/Presentation/SpotList/Type/SpotListItemSizeType.swift
+++ ACON-iOS/ACON-iOS/Presentation/SpotList/Type/SpotListItemSizeType.swift
@@ -9,12 +9,13 @@ import Foundation
 
 enum SpotListItemSizeType {
     
-    case minimumLineSpacing, itemWidth
+    case minimumLineSpacing, itemWidth, headerHeight
     
     var value: CGFloat {
         switch self {
         case .minimumLineSpacing: return 12
         case .itemWidth: return ScreenUtils.width - 40
+        case .headerHeight: return 38
         }
     }
     
