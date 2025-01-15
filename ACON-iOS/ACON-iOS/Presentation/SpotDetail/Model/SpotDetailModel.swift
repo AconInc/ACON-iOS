832e7e3fd6e64adf8537a472e6aafabef58c31fb
cirtuare <cirtuare@gmail.com>
2025-01-16 02:01:39 +0900

    [Feat] SpotDetailModel 구현 (#27)

---
+++ ACON-iOS/ACON-iOS/Presentation/SpotDetail/Model/SpotDetailModel.swift
@@ -0,0 +1,51 @@
+//
+//  SpotDetailModel.swift
+//  ACON-iOS
+//
+//  Created by 이수민 on 1/16/25.
+//
+
+import Foundation
+import UIKit
+
+struct SpotDetail: Equatable {
+    
+    let firstImageURL: String
+    
+    let isOpen: Bool
+    
+    let address: String
+    
+    let localAcornCount: Int
+    
+    let basicAcornCount: Int
+    
+    init(firstImageURL: String, isOpen: Bool, address: String, localAcornCount: Int, basicAcornCount: Int) {
+        self.firstImageURL = firstImageURL
+        self.isOpen = isOpen
+        self.address = address
+        self.localAcornCount = localAcornCount
+        self.basicAcornCount = basicAcornCount
+    }
+    
+}
+
+
+struct SpotMenuInfo {
+    
+    let menuID: Int
+    
+    let name: String
+    
+    let price: Int
+    
+    let imageURL: String
+    
+    init(menuID: Int, name: String, price: Int, imageURL: String) {
+        self.menuID = menuID
+        self.name = name
+        self.price = price
+        self.imageURL = imageURL
+    }
+    
+}
