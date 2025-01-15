22d86be4d6f0fcf12e46db4a0f4af166d78b66fa
cirtuare <cirtuare@gmail.com>
2025-01-16 07:13:37 +0900

    [Feat] SpotDetailVM 구현 (#27)

---
+++ ACON-iOS/ACON-iOS/Presentation/SpotDetail/ViewModel/SpotDetailViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  SpotDetailViewModel.swift
+//  ACON-iOS
+//
+//  Created by 이수민 on 1/16/25.
+//
+
+import Foundation
+
+class SpotDetailViewModel {
+    
+    let menuDummyData: [SpotMenuInfo] = [
+        SpotMenuInfo(menuID: 1, name: "마라탕", price: 10000, imageURL: ""),
+        SpotMenuInfo(menuID: 1, name: "꿔바로우", price: 22000, imageURL: ""),
+        SpotMenuInfo(menuID: 1, name: "마라탕", price: 10000, imageURL: ""),
+        SpotMenuInfo(menuID: 1, name: "꿔바로우", price: 22000, imageURL: ""),
+        SpotMenuInfo(menuID: 1, name: "마라탕", price: 10000, imageURL: ""),
+        SpotMenuInfo(menuID: 1, name: "마라탕", price: 10000, imageURL: ""),
+        SpotMenuInfo(menuID: 1, name: "마라탕", price: 10000, imageURL: ""),
+        SpotMenuInfo(menuID: 1, name: "꿔바로우", price: 22000, imageURL: ""),
+        SpotMenuInfo(menuID: 1, name: "마라탕", price: 10000, imageURL: ""),
+    ]
+    
+}
