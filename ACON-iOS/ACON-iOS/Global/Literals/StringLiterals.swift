e4909bebe222dccdba6fa8a7bb46b1e10bac2a9e
Yurim Kim <yurim830@gmail.com>
2025-01-17 02:59:25 +0900

    [Chore] 슬라이더 라벨 StringLiterals 추가 (#33)

--- ACON-iOS/ACON-iOS/Global/Literals/StringLiterals.swift
+++ ACON-iOS/ACON-iOS/Global/Literals/StringLiterals.swift
@@ -148,6 +148,11 @@ enum StringLiterals {
         
         static let visitPurposeSection = "방문 목적"
         
+        static let fiveStepMinutes = ["5분 이내", "10분", "15분", "20분", "20분 이상"]
+        
+        static let restaurantPrices = ["5천원 이하", "1만원", "3만원", "5만원", "5만원 이상"]
+        
+        static let cafePrices = ["3천원 이하", "5천원", "1만원 이상"]
     }
     
 }
