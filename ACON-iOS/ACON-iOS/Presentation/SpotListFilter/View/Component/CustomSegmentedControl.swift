39282daa356d58ce4fc317202104db6a51a01441
Yurim Kim <yurim830@gmail.com>
2025-01-16 14:46:57 +0900

    [Add] CustomSegment 생성 (#29)
    미완성

--- ACON-iOS/ACON-iOS/Presentation/SpotListFilter/View/Component/CustomSegmentedControl.swift
+++ ACON-iOS/ACON-iOS/Presentation/SpotListFilter/View/Component/CustomSegmentedControl.swift
@@ -7,26 +7,34 @@
 
 import UIKit
 
+import Then
+
 // TODO: 커스텀 세그먼트컨트롤 만들기...
 /// - 배경 insets
 /// - attributed string
 
 class CustomSegmentedControl: UISegmentedControl {
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        self.removeBackgroundAndDivider()
-    }
+
     
-    override init(items: [Any]?) {
-        super.init(items: items)
-        self.removeBackgroundAndDivider()
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+////        self.removeBackgroundAndDivider()
+//    }
+    
+    init() {
+        let segmentItems = [StringLiterals.SpotListFilter.restaurant,
+                            StringLiterals.SpotListFilter.cafe]
+        super.init(items: segmentItems)
+        
+        setStyle()
     }
     
     required init?(coder: NSCoder) {
         fatalError()
     }
     
+    // TODO: 커스텀
     private func removeBackgroundAndDivider() {
         let image = UIImage()
         self.setBackgroundImage(image, for: .normal, barMetrics: .default)
@@ -36,4 +44,13 @@ class CustomSegmentedControl: UISegmentedControl {
         self.setDividerImage(image, forLeftSegmentState: .selected, rightSegmentState: .normal, barMetrics: .default)
     }
     
+    private func setStyle() {
+        self.do {
+            $0.selectedSegmentIndex = 0
+            $0.selectedSegmentTintColor = .acWhite
+            $0.backgroundColor = .gray8
+            $0.setTitleTextAttributes(String.ACStyle(.s2, .gray5), for: .normal)
+            $0.setTitleTextAttributes(String.ACStyle(.s2, .gray9), for: .selected)
+        }
+    }
 }
