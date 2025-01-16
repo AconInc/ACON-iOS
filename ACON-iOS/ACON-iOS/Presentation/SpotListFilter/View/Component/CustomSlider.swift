f6a007953c0a40f0cf0b40d55e33842aaf117a1d
Yurim Kim <yurim830@gmail.com>
2025-01-17 01:08:01 +0900

    [Add] main_org50 추가 (#33)

---
+++ ACON-iOS/ACON-iOS/Presentation/SpotListFilter/View/Component/CustomSlider.swift
@@ -0,0 +1,20 @@
+//
+//  CustomSlider.swift
+//  ACON-iOS
+//
+//  Created by 김유림 on 1/17/25.
+//
+
+import UIKit
+
+class CustomSlider: BaseView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
