fbbbd2f6c97c18da565b8c30433c9c1fb5a868ea
Yurim Kim <yurim830@gmail.com>
2025-01-18 06:29:36 +0900

    [Chore] 변수명 변경 (#33)

--- ACON-iOS/ACON-iOS/Global/Literals/StringLiterals.swift
+++ ACON-iOS/ACON-iOS/Global/Literals/StringLiterals.swift
@@ -148,7 +148,7 @@ enum StringLiterals {
         
         static let visitPurposeSection = "방문 목적"
         
-        static let fiveStepMinutes = ["5분 이내", "10분", "15분", "20분", "20분 이상"]
+        static let walkingTimes = ["5분 이내", "10분", "15분", "20분", "20분 이상"]
         
         static let restaurantPrices = ["5천원 이하", "1만원", "3만원", "5만원", "5만원 이상"]
         
