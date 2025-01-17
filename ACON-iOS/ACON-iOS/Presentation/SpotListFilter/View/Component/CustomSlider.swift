70371c513227f9520aa3ad06e2ea570c691038ed
Yurim Kim <yurim830@gmail.com>
2025-01-18 05:27:57 +0900

    [Feat] 커스텀 슬라이더 구현 (#33)

--- ACON-iOS/ACON-iOS/Presentation/SpotListFilter/View/Component/CustomSlider.swift
+++ ACON-iOS/ACON-iOS/Presentation/SpotListFilter/View/Component/CustomSlider.swift
@@ -3,159 +3,227 @@
 //  ACON-iOS
 //
 //  Created by 김유림 on 1/17/25.
-//
 
 import UIKit
 
-class CustomSlider: BaseView {
+protocol SliderViewDelegate: AnyObject {
+    func sliderView(_ sender: CustomSlider, changedValue value: Int)
+}
+
+final class CustomSlider: BaseView {
     
-    // MARK: - Properties
+    // MARK: - Basic Properties
     
-    private var stepTexts: [String]
-    private lazy var steps: [CGFloat] = divideHundred(by: stepTexts.count)
+    weak var delegate: SliderViewDelegate?
     
-    private let trackView = UIView()  // 슬라이더 배경
-    private let fillView = UIView()  // 채워지는 부분
-    private let thumbView = UIView() // 손잡이
-    private var labels: [UILabel] = [] // 라벨
-    private let labelStackView = UIStackView()
+    override var intrinsicContentSize: CGSize {
+        return .init(width: .zero, height: thumbSize)
+    }
     
-    private var currentValue: CGFloat = 0 // 현재 값
+    var value: Int = 1 {
+        didSet {
+            delegate?.sliderView(self, changedValue: value)
+        }
+    }
     
     
-    // MARK: - Initializer
+    //MARK: - UI Properties
     
-    init(range: [String]) {
+    private let trackView = UIView()
+    
+    private let fillTrackView = UIView()
+    
+    private lazy var thumbView = UIView()
+    
+    private var indicatorLabels: [UILabel] = [] // 라벨
+    
+    
+    // MARK: - UI helping Properties
+    
+    private var indicators: [String]
+    
+    private var startIndex: Int
+    
+    private var touchBeganPosX: CGFloat?
+    
+    private var didLayoutSubViews: Bool = false // NOTE: 1번만 실행되도록
+    
+    private lazy var slicedPosX: CGFloat = 0 // layoutSubviews 단계에서 값 지정
+    
+    private let thumbSize: CGFloat = 22
+    
+    private let labelWidth: CGFloat = 50
+    
+    
+    // MARK: - LifeCycle
+    
+    init(indicators: [String], startIndex: Int) {
+        self.indicators = indicators
+        self.startIndex = startIndex
+        
         super.init(frame: .zero)
     }
     
-    @MainActor required init?(coder: NSCoder) {
+    required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     override func setHierarchy() {
-        self.addSubviews(trackView,
-                         fillView,
-                         thumbView,
-                         labelStackView)
+        addSubviews(
+            trackView,
+            fillTrackView,
+            thumbView
+        )
+        
     }
     
     override func setLayout() {
-        super.setLayout()
-        
-        // 레이아웃 설정
         trackView.snp.makeConstraints {
-            $0.height.equalTo(10)
-            $0.top.equalToSuperview().offset(6)
-            $0.horizontalEdges.equalToSuperview().inset(7)
+            $0.horizontalEdges.equalToSuperview().inset(thumbSize / 2)
+            $0.verticalEdges.equalToSuperview()
         }
         
-        fillView.snp.makeConstraints {
-            $0.leading.verticalEdges.equalTo(trackView)
+        fillTrackView.snp.makeConstraints {
+            $0.left.equalTo(trackView)
+            $0.top.bottom.equalTo(trackView)
             $0.width.equalTo(0)
         }
         
-        // 손잡이 위치 초기화
-        let thumbSize: CGFloat = 22
-        let thumbX = positionForValue(currentValue)
-        
         thumbView.snp.makeConstraints {
-            $0.size.equalTo(thumbSize)
-            $0.leading.equalToSuperview().offset(thumbX - thumbSize / 2)
             $0.centerY.equalTo(trackView)
-        }
-        
-        // 라벨 위치 설정
-        labelStackView.snp.makeConstraints {
-            $0.top.equalTo(trackView.snp.bottom).offset(10)
-            $0.horizontalEdges.equalToSuperview()
-            $0.bottom.equalToSuperview()
+            $0.left.equalTo(trackView)
+            $0.size.equalTo(thumbSize)
         }
     }
     
     override func setStyle() {
-        // 배경 뷰 설정
-        trackView.do {
-            $0.backgroundColor = .gray8
-            $0.layer.cornerRadius = 8
-        }
+        trackView.backgroundColor = .gray8
         
-        // 채워지는 뷰 설정
-        fillView.do {
-            $0.backgroundColor = .mainOrg50
-            $0.layer.cornerRadius = 8
-        }
+        fillTrackView.backgroundColor = .mainOrg50
         
-        // 손잡이 설정
         thumbView.do {
-            $0.backgroundColor = .acWhite
-            $0.layer.cornerRadius = 11
-            $0.layer.shadowColor = UIColor.acBlack.cgColor
-            $0.layer.shadowOpacity = 0.1
-            $0.layer.shadowOffset = CGSize(width: 0, height: 2)
+            $0.backgroundColor = .systemBackground
+            $0.isUserInteractionEnabled = true
+            $0.layer.shadowColor = UIColor.gray.cgColor
+            $0.layer.shadowOffset = .init(width: 3, height: 3)
+            $0.layer.shadowRadius = 8
+            $0.layer.shadowOpacity = 0.8
+            
+            addGestureRegocnizer()
         }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+//        updateLayoutToStartingPoint() // TODO: 이걸 넣으면 터치가 안 되는 문제 있음. 추후 수정 예정
         
-        // 라벨 생성
-        for value in stepTexts {
-            let label = UILabel()
-            label.setLabel(
-                text: value,
-                style: .c1,
-                color: .gray4
-            )
-            labels.append(label)
-            labelStackView.addArrangedSubview(label)
-        }
-        
-        // 제스처 추가
-        let panGesture = UIPanGestureRecognizer(
-            target: self,
-            action: #selector(handlePan(_:)))
-        thumbView.addGestureRecognizer(panGesture)
+        updateStyle()
     }
     
 }
 
 
-// MARK: - @objc function
+// MARK: - Late UI Settings
 
 private extension CustomSlider {
     
-    @objc func handlePan(_ gesture: UIPanGestureRecognizer) {
-        let translation = gesture.translation(in: self)
-        let thumbCenterX = thumbView.center.x + translation.x
-        let clampedX = max(trackView.frame.minX, min(thumbCenterX, trackView.frame.maxX))
+    func updateLayoutToStartingPoint() {
+        let startPosX = CGFloat(Int(slicedPosX) * startIndex - Int(thumbSize) / 2)
+        touchBeganPosX = startPosX
         
-        // 현재 값 계산
-        let percentage = (clampedX - trackView.frame.minX) / trackView.frame.width
-        let nearestValue = nearestSnapValue(percentage * 100)
-        currentValue = nearestValue
+        thumbView.snp.updateConstraints {
+            $0.left.equalTo(trackView).offset(startPosX)
+        }
         
-        // 레이아웃 업데이트
-        gesture.setTranslation(.zero, in: self)
-        setNeedsLayout()
+        fillTrackView.snp.updateConstraints {
+            $0.width.equalTo(startPosX)
+        }
+    }
+    
+    func updateStyle() {
+        if !didLayoutSubViews {
+            setIndicators()
+            thumbView.layer.cornerRadius = thumbView.frame.width / 2
+            thumbView.layer.shadowPath = UIBezierPath(
+                roundedRect: thumbView.bounds,
+                cornerRadius: thumbView.layer.cornerRadius
+            ).cgPath
+        }
     }
     
 }
 
 
-// MARK: - Helper
+// MARK: - Helpers
+
 private extension CustomSlider {
     
-    func divideHundred(by number: Int) -> [CGFloat] {
-        guard number > 0 else { return [] }
+    private func setIndicators() {
+        self.slicedPosX = trackView.frame.width / CGFloat(indicators.count - 1)
         
-        let step = 100 / number
-        return (0...number).map { CGFloat($0 * step) }
+        for (i, text) in indicators.enumerated() {
+            let posX = slicedPosX * CGFloat(i)
+            let label = makeIndicatorLabel(text)
+            
+            addSubview(label)
+            label.snp.makeConstraints {
+                $0.top.equalTo(trackView).offset(10)
+                $0.centerX.equalTo(trackView).offset(posX)
+                $0.width.equalTo(labelWidth)
+                $0.bottom.equalToSuperview()
+            }
+        }
+        
+        didLayoutSubViews.toggle()
     }
     
-    private func positionForValue(_ value: CGFloat) -> CGFloat {
-        let percentage = value / 100
-        return trackView.frame.minX + percentage * trackView.frame.width
+    private func makeIndicatorLabel(_ text: String) -> UILabel {
+        let label = UILabel().then {
+            $0.setLabel(text: text, style: .c1, color: .gray4)
+            indicatorLabels.append($0)
+        }
+        return label
     }
     
-    private func nearestSnapValue(_ value: CGFloat) -> CGFloat {
-        return values.min(by: { abs($0 - value) < abs($1 - value) }) ?? 0
+    private func addGestureRegocnizer() {
+        let gesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        thumbView.addGestureRecognizer(gesture)
+    }
+    
+}
+
+
+//MARK: - @objc functions
+
+extension CustomSlider {
+    
+    @objc func handlePan(_ recognizer: UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: thumbView)
+        
+        if recognizer.state == .began {
+            // 팬 제스쳐가 시작된 x좌표 저장
+            touchBeganPosX = thumbView.frame.minX
+        }
+        
+        if recognizer.state == .changed {
+            guard let startX = self.touchBeganPosX else { return }
+            
+            var offSet = startX + translation.x // 시작지점 + 제스쳐 거리 = 현재 제스쳐 좌표
+            if offSet < 0 || offSet > trackView.frame.width { return } // 제스쳐가 trackView의 범위를 벗어나는 경우 무시
+            let slicedPosX = trackView.frame.width / CGFloat(indicators.count - 1) // maxValue를 기준으로 trackView를 n등분
+            
+            // value = 반올림(현재 제스쳐 좌표 / 1단위의 크기) -> 슬라이더의 값이 변할 때마다 똑똑 끊기는 효과를 주기 위해
+            let newValue = round(offSet / slicedPosX)
+            offSet = slicedPosX * newValue - (thumbSize / 2)
+            
+            thumbView.snp.updateConstraints {
+                $0.left.equalTo(trackView).offset(offSet)
+            }
+            
+            fillTrackView.snp.updateConstraints {
+                $0.width.equalTo(offSet)
+            }
+        }
     }
     
 }
