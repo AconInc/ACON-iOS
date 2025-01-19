f0a023aacb9756bf98a2b4ed3ca0b19be99ea4a0
Ohjackson <wogus3673@naver.com>
2025-01-20 05:35:29 +0900

    [Chore] change root (#54)

--- ACON-iOS/ACON-iOS/Application/SceneDelegate.swift
+++ ACON-iOS/ACON-iOS/Application/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         self.window = UIWindow(windowScene: windowScene)
-        let navigationController = UINavigationController(rootViewController: OnboardingViewController())
+        let navigationController = UINavigationController(rootViewController: ACTabBarController())
         navigationController.navigationBar.isHidden = true
         self.window?.rootViewController = navigationController
         self.window?.makeKeyAndVisible()
