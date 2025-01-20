f54cef778828555903b548d82b3b09502b815764
Yurim Kim <yurim830@gmail.com>
2025-01-20 15:00:15 +0900

    [Feat] SpotListFilterVM 세팅 (#62)

--- ACON-iOS/ACON-iOS/Presentation/SpotListFilter/ViewModel/SpotListFilterViewModel.swift
+++ ACON-iOS/ACON-iOS/Presentation/SpotListFilter/ViewModel/SpotListFilterViewModel.swift
@@ -5,6 +5,7 @@
 //  Created by 김유림 on 1/14/25.
 //
 
+import CoreLocation
 import Foundation
 
 class SpotListFilterViewModel {
@@ -13,6 +14,27 @@ class SpotListFilterViewModel {
     
     var spotType: ObservablePattern<SpotType> = ObservablePattern(.restaurant)
     
+    var userCoordinate: CLLocationCoordinate2D?
     
+    var filter: SpotFilterModel = .init(
+        latitude: 0,
+        longitude: 0,
+        condition: SpotConditionModel(
+            spotType: SpotType.restaurant.text,
+            filterList: [],
+            walkingTime: -1,
+            priceRange: -1
+        )
+    )
     
+}
+
+extension SpotListFilterViewModel: ACLocationManagerDelegate {
+
+    func locationManager(_ manager: ACLocationManager, didUpdateLocation coordinate: CLLocationCoordinate2D) {
+        print("성공 - 위도: \(coordinate.latitude), 경도: \(coordinate.longitude)")
+        self.userCoordinate = coordinate
+//        pushToLocalMapVC()
+    }
+
 }
