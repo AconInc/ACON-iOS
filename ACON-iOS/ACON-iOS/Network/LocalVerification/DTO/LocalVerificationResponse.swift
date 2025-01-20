7c09da8075cff59608105d4838aee863ced981ad
cirtuare <cirtuare@gmail.com>
2025-01-20 15:14:45 +0900

    [Feat] LocalVerificationResponseDTO 구현 (#70)

---
+++ ACON-iOS/ACON-iOS/Network/LocalVerification/DTO/LocalVerificationResponse.swift
@@ -0,0 +1,15 @@
+//
+//  LocalVerificationResponse.swift
+//  ACON-iOS
+//
+//  Created by 이수민 on 1/20/25.
+//
+
+import Foundation
+
+
+struct LocalVerificationResponse: Codable {
+    
+    let area: String
+    
+}
