1869f0fb51d5681d46eefd9ae1df000c9da5f11c
cirtuare <cirtuare@gmail.com>
2025-01-20 15:18:57 +0900

    [Feat] LocalVerificationTargetType 구현 (#70)

---
+++ ACON-iOS/ACON-iOS/Network/LocalVerification/LocalVerificationTargetType.swift
@@ -0,0 +1,45 @@
+//
+//  LocalVerificationTargetType.swift
+//  ACON-iOS
+//
+//  Created by 이수민 on 1/20/25.
+//
+
+import Foundation
+
+import Moya
+
+enum LocalVerificationTargetType {
+    
+    case postLocalArea
+    
+}
+
+extension LocalVerificationTargetType: TargetType {
+
+    var method: Moya.Method {
+        switch self {
+        case .postLocalArea:
+            return .post
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .postLocalArea:
+            return utilPath + "member/area"
+        }
+    }
+    
+    var task: Task {
+        return .requestPlain
+    }
+    
+    var headers: [String : String]? {
+        // TODO: - 추후 유저디폴트 토큰으로 변경
+        let token = ""
+        let headers = HeaderType.headerWithToken(token: "Bearer " + token)
+        return headers
+    }
+    
+}
