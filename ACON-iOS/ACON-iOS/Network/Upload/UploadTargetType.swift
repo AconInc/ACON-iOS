c0c84a2693186a235e50ed404a8912789a246e6e
cirtuare <cirtuare@gmail.com>
2025-01-21 01:24:37 +0900

    [Feat] UploadTargetType 구현 (#71)

---
+++ ACON-iOS/ACON-iOS/Network/Upload/UploadTargetType.swift
@@ -0,0 +1,86 @@
+//
+//  UploadTargetType.swift
+//  ACON-iOS
+//
+//  Created by 이수민 on 1/21/25.
+//
+
+import Foundation
+
+import Moya
+
+enum UploadTargetType {
+    
+    case getSearchSuggestion(parameter: GetSearchSuggestionRequest)
+    case postReviewRequest
+    case getSearchKeyword(parameter: GetSearchKeywordRequest)
+    case getReviewVerification(parameter: GetReviewVerificationRequest)
+    case getAcornCount
+    
+}
+
+extension UploadTargetType: TargetType {
+
+    var method: Moya.Method {
+        switch self {
+        case .postReviewRequest:
+            return .post
+        default:
+            return .get
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .getSearchSuggestion:
+            return utilPath + "/search-suggestions"
+        case .postReviewRequest:
+            return utilPath + "/review"
+        case .getSearchKeyword:
+            return utilPath + "/spots/search"
+        case .getReviewVerification:
+            return utilPath + "/spot/verify"
+        case .getAcornCount:
+            return utilPath + "/member/acorn"
+        }
+    }
+    
+    var parameter: [String : Any]?  {
+        switch self {
+        case .getSearchSuggestion(let parameter):
+            return ["latitude": parameter.latitude, "longitude": parameter.longitude]
+        case .getSearchKeyword(let parameter):
+            return ["keyword": parameter.keyword]
+        case .getReviewVerification(let parameter):
+            return ["spotId": parameter.spotId,
+                    "latitude": parameter.latitude,
+                    "longitude": parameter.longitude]
+        default:
+            return .none
+        }
+    }
+    
+    var task: Task {
+        if let parameter = parameter {
+            return .requestParameters(parameters: parameter, encoding: URLEncoding.default)
+        } else {
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        var headers = HeaderType.noHeader
+        switch self {
+        case .getSearchKeyword, .getReviewVerification:
+            headers = HeaderType.noHeader
+        case .getSearchSuggestion:
+            headers = HeaderType.basicHeader
+        case .postReviewRequest, .getAcornCount:
+            // TODO: - 추후 유저디폴트 토큰으로 변경
+            let token = ""
+            headers = HeaderType.headerWithToken(token: "Bearer " + token)
+        }
+        return headers
+    }
+    
+}
