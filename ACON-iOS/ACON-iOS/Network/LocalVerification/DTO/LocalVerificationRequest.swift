cac377b9c9bbce82ac57a896aaefe2109e918c1b
cirtuare <cirtuare@gmail.com>
2025-01-20 15:13:49 +0900

    [Feat] LocalVerificationRequestDTO 구현 (#70)

---
+++ ACON-iOS/ACON-iOS/Network/LocalVerification/DTO/LocalVerificationRequest.swift
@@ -0,0 +1,17 @@
+//
+//  LocalVerificationRequest.swift
+//  ACON-iOS
+//
+//  Created by 이수민 on 1/20/25.
+//
+
+import Foundation
+
+
+struct LocalVerificationRequest: Codable {
+    
+    let latitude: Double
+    
+    let longitude: Double
+    
+}
