967d6a108fcc4d942d0c9fceae3808622dfa9027
cirtuare <cirtuare@gmail.com>
2025-01-22 02:21:09 +0900

    [Feat] VM - getSearchSuggestion 메소드 구현 (#71)

--- ACON-iOS/ACON-iOS/Presentation/Upload/ViewModel/SpotSearchViewModel.swift
+++ ACON-iOS/ACON-iOS/Presentation/Upload/ViewModel/SpotSearchViewModel.swift
@@ -9,19 +9,25 @@ import Foundation
 
 class SpotSearchViewModel {
     
+    var longitude: Double
+    
+    var latitude: Double
+    
     let onSuccessGetSearchSuggestion: ObservablePattern<Bool> = ObservablePattern(nil)
     
-    var searchSuggestionData: ObservablePattern<SearchSuggestionModel> = ObservablePattern(nil)
+    var searchSuggestionData: ObservablePattern<[SearchSuggestionModel]> = ObservablePattern(nil)
     
     let onSuccessGetSearchKeyword: ObservablePattern<Bool> = ObservablePattern(nil)
     
     var searchKeywordData: ObservablePattern<[SearchKeywordModel]> = ObservablePattern(nil)
     
-    let searchSuggestionDummyData: SearchSuggestionModel = SearchSuggestionModel(spotList: ["하이디라오", "신의주찹쌀순대", "뭐시기저시기", "카이센동우니도", "하잉"])
+//    let searchSuggestionDummyData: SearchSuggestionModel = SearchSuggestionModel(spotList: ["하이디라오", "신의주찹쌀순대", "뭐시기저시기", "카이센동우니도", "하잉"])
     
-    init() {
-        self.searchSuggestionData.value = searchSuggestionDummyData
-        self.onSuccessGetSearchSuggestion.value = true
+    init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+//        self.searchSuggestionData.value = searchSuggestionDummyData
+//        self.onSuccessGetSearchSuggestion.value = true
 //        self.searchKeywordData.value = searchKeywordDummyData
 //        self.onSuccessGetSearchKeyword.value = true
     }
@@ -45,6 +51,26 @@ class SpotSearchViewModel {
             default:
                 print("VM - Fail to getSearchKeyword")
                 self?.onSuccessGetSearchKeyword.value = false
+                return
+            }
+        }
+    }
+    
+    func getSearchSuggestion() {
+        let parameter = GetSearchSuggestionRequest(latitude: latitude, longitude: longitude)
+        
+        ACService.shared.uploadService.getSearchSuggestion(parameter: parameter) { [weak self] response in
+            switch response {
+            case .success(let data):
+                let searchSuggestionData = data.suggestionList.map { suggestion in
+                    return SearchSuggestionModel(spotId: suggestion.spotId,
+                                                 spotName: suggestion.spotName)
+                }
+                self?.searchSuggestionData.value = searchSuggestionData
+                self?.onSuccessGetSearchSuggestion.value = true
+            default:
+                print("VM - Fail to getSearchSuggestion")
+                self?.onSuccessGetSearchSuggestion.value = false
                 return
             }
         }
