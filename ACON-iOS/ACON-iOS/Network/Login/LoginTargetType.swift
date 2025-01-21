da66df70eb14244e127171adb417643bc8d8486d
cirtuare <cirtuare@gmail.com>
2025-01-22 02:39:30 +0900

    [Feat] LoginTargetType 구현 (#88)

---
+++ ACON-iOS/ACON-iOS/Network/Login/LoginTargetType.swift
@@ -0,0 +1,50 @@
+//
+//  LoginTargetType.swift
+//  ACON-iOS
+//
+//  Created by 이수민 on 1/22/25.
+//
+
+import Foundation
+
+import Moya
+
+enum LoginTargetType {
+
+    case postLogin(_ requestBody: PostLoginRequest)
+    
+}
+
+extension LoginTargetType: TargetType {
+
+    var method: Moya.Method {
+        switch self {
+        case .postLogin:
+            return .post
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .postLogin:
+            return utilPath + "auth/login"
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .postLogin(let requestBody):
+            return .requestJSONEncodable(requestBody)
+        }
+    }
+    
+    var headers: [String : String]? {
+        var headers = HeaderType.basicHeader
+        switch self {
+        case .postLogin:
+            headers = HeaderType.basicHeader
+        }
+        return headers
+    }
+    
+}
