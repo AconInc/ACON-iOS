cebe962f59295a4128faa9a977e03ebd085c830e
Yurim Kim <yurim830@gmail.com>
2025-01-21 21:27:12 +0900

    [Chore] ItemHeight 추가 (#86)

--- ACON-iOS/ACON-iOS/Presentation/SpotList/Type/SpotListItemSizeType.swift
+++ ACON-iOS/ACON-iOS/Presentation/SpotList/Type/SpotListItemSizeType.swift
@@ -9,12 +9,14 @@ import Foundation
 
 enum SpotListItemSizeType {
     
-    case minimumLineSpacing, itemWidth, headerHeight
+    case minimumLineSpacing, itemWidth, longItemHeight, shortItemHeight, headerHeight
     
     var value: CGFloat {
         switch self {
         case .minimumLineSpacing: return 12
         case .itemWidth: return ScreenUtils.width - 40
+        case .longItemHeight: return 408
+        case .shortItemHeight: return 128
         case .headerHeight: return 38
         }
     }
