ce5869b064d6b6261ccc23c4bd5257d9d30cf907
Yurim Kim <yurim830@gmail.com>
2025-01-22 06:30:41 +0900

    [Feat] 로그인 모달 뷰 구현 완료 (#96)

--- ACON-iOS/ACON-iOS/Presentation/LoginModal/View/LoginModalView.swift
+++ ACON-iOS/ACON-iOS/Presentation/LoginModal/View/LoginModalView.swift
@@ -7,14 +7,164 @@
 
 import UIKit
 
-class LoginModalView: BaseView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class LoginModalView: GlassmorphismView {
+    
+    // MARK: - UI Properties
+    
+    private let titleLabel = UILabel()
+    
+    private var subTitleLabel  = UILabel()
+    
+    var googleLoginButton = UIButton()
+    
+    var appleLoginButton = UIButton()
+    
+    private let youAgreedLabel = UILabel()
+    
+    private let termsStackView = UIStackView()
+    
+    var termsOfUseLabel = UILabel()
+    
+    var privacyPolicyLabel = UILabel()
+    
+    var socialLoginButtonConfiguration: UIButton.Configuration = {
+        var configuration = UIButton.Configuration.plain()
+        configuration.imagePlacement = .leading
+        configuration.imagePadding = 4
+        configuration.titleAlignment = .center
+        configuration.preferredSymbolConfigurationForImage = UIImage.SymbolConfiguration(pointSize: 24)
+        configuration.contentInsets = NSDirectionalEdgeInsets(top: 12, leading: 93, bottom: 12, trailing: 93)
+        return configuration
+    }()
+    
+    
+    // MARK: - Lifecycle
+    
+    override func setHierarchy() {
+        super.setHierarchy()
+        
+        self.addSubviews(
+            titleLabel,
+            subTitleLabel,
+            googleLoginButton,
+            appleLoginButton,
+            youAgreedLabel,
+            termsStackView
+        )
+        
+        termsStackView.addArrangedSubviews(
+            termsOfUseLabel,
+            privacyPolicyLabel
+        )
     }
-    */
-
+    
+    override func setLayout() {
+        super.setLayout()
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(ScreenUtils.heightRatio * 86)
+            $0.centerX.equalToSuperview()
+        }
+        
+        subTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(8)
+            $0.centerX.equalToSuperview()
+        }
+        
+        googleLoginButton.snp.makeConstraints {
+            $0.top.equalTo(subTitleLabel.snp.bottom).offset(ScreenUtils.heightRatio * 64)
+            $0.height.equalTo(ScreenUtils.heightRatio * 48)
+            $0.horizontalEdges.equalToSuperview().inset(ScreenUtils.widthRatio*20)
+        }
+        
+        appleLoginButton.snp.makeConstraints {
+            $0.top.equalTo(googleLoginButton.snp.bottom).offset(8)
+            $0.height.horizontalEdges.equalTo(googleLoginButton)
+        }
+        
+        youAgreedLabel.snp.makeConstraints {
+            $0.top.equalTo(appleLoginButton.snp.bottom).offset(ScreenUtils.heightRatio * 16)
+            $0.centerX.equalToSuperview()
+        }
+        
+        termsStackView.snp.makeConstraints {
+            $0.top.equalTo(youAgreedLabel.snp.bottom).offset(ScreenUtils.heightRatio * 4)
+            $0.centerX.equalToSuperview()
+        }
+        
+        termsOfUseLabel.snp.makeConstraints {
+            $0.width.equalTo(48)
+        }
+        
+        privacyPolicyLabel.snp.makeConstraints {
+            $0.width.equalTo(95)
+        }
+    }
+    
+    override func setStyle() {
+        super.setStyle()
+        
+        self.setHandlerImageView()
+        
+        titleLabel.setLabel(text: StringLiterals.LoginModal.title,
+                            style: .h5,
+                            color: .acWhite,
+                            alignment: .center,
+                            numberOfLines: 1)
+        
+        subTitleLabel.setLabel(text: StringLiterals.LoginModal.subTitle,
+                               style: .b1,
+                               color: .gray2,
+                               alignment: .center,
+                        numberOfLines: 2)
+        
+        googleLoginButton.do {
+            $0.configuration = socialLoginButtonConfiguration
+            $0.backgroundColor = .gray1
+            $0.roundedButton(cornerRadius: 6, maskedCorners: [.layerMaxXMaxYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMinXMinYCorner])
+            $0.setImage(.googleLogo, for: .normal)
+            $0.setAttributedTitle(text: StringLiterals.Login.googleLogin,
+                                  style: .s1,
+                                  color: .acBlack)
+        }
+        
+        appleLoginButton.do {
+            $0.configuration = socialLoginButtonConfiguration
+            $0.backgroundColor = .gray9
+            $0.roundedButton(cornerRadius: 6, maskedCorners: [.layerMaxXMaxYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMinXMinYCorner])
+            $0.setImage(.appleLogo, for: .normal)
+            $0.setAttributedTitle(text: StringLiterals.Login.appleLogin,
+                                  style: .s1,
+                                  color: .acWhite)
+    
+        }
+        
+        youAgreedLabel.do {
+            $0.setLabel(text: StringLiterals.Login.youAgreed,
+                        style: .b2,
+                        color: .gray3)
+        }
+        
+        termsStackView.do {
+            $0.axis = .horizontal
+            $0.spacing = 16
+        }
+        
+        termsOfUseLabel.do {
+            $0.setLabel(text: StringLiterals.Login.termsOfUse,
+                        style: .b2,
+                        color: .gray5)
+            $0.setUnderline(range: NSRange(location: 0, length: termsOfUseLabel.text?.count ?? 4))
+            $0.isUserInteractionEnabled = true
+        }
+        
+        privacyPolicyLabel.do {
+            $0.setLabel(text: StringLiterals.Login.privacyPolicy,
+                        style: .b2,
+                        color: .gray5)
+            $0.setUnderline(range: NSRange(location: 0, length: privacyPolicyLabel.text?.count ?? 8))
+            $0.isUserInteractionEnabled = true
+        }
+    }
+    
 }
