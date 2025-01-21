b9d5b4a07a9c307811b82c66335d7c7a8779aa99
Yurim Kim <yurim830@gmail.com>
2025-01-21 16:43:57 +0900

    [Add] CustomRefreshControl 파일 생성 (#82)

---
+++ ACON-iOS/ACON-iOS/Presentation/SpotList/View/Component/CustomRefreshControl.swift
@@ -0,0 +1,18 @@
+//
+//  CustomRefreshControl.swift
+//  ACON-iOS
+//
+//  Created by 김유림 on 1/21/25.
+//
+
+import UIKit
+
+import Lottie
+
+class CustomRefreshControl: UIRefreshControl {
+    
+//    private let animationView = Lottie.AnimationView(name: "pull.to.refresh.start.animation")
+//    private let maxPullDistance: CGFloat = 150
+    
+    
+}
