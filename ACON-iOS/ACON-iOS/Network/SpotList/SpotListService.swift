024bbc1a1337aa01cd414739e93bbfdf4f2f2e5c
Yurim Kim <yurim830@gmail.com>
2025-01-22 05:05:22 +0900

    [Chore] 줄바꿈 (#87)

--- ACON-iOS/ACON-iOS/Network/SpotList/SpotListService.swift
+++ ACON-iOS/ACON-iOS/Network/SpotList/SpotListService.swift
@@ -16,13 +16,17 @@ protocol SpotListServiceProtocol {
 }
 
 final class SpotListService: BaseService<SpotListTargetType>, SpotListServiceProtocol{
-
+    
     func postSpotList(requestBody: PostSpotListRequest,
                       completion: @escaping (NetworkResult<PostSpotListResponse>) -> Void) {
         self.provider.request(.postSpotList(requestBody)) { result in
             switch result {
             case .success(let response):
-                let networkResult: NetworkResult<PostSpotListResponse> = self.judgeStatus(statusCode: response.statusCode, data: response.data, type: PostSpotListResponse.self)
+                let networkResult: NetworkResult<PostSpotListResponse> = self.judgeStatus(
+                    statusCode: response.statusCode,
+                    data: response.data,
+                    type: PostSpotListResponse.self
+                )
                 completion(networkResult)
             case .failure(let errorResponse):
                 print(errorResponse)
