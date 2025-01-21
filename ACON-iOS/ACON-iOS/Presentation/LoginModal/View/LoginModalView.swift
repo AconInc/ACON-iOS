8744bab0c1cbd7ba2489def6acd190308fe18925
Yurim Kim <yurim830@gmail.com>
2025-01-22 05:09:23 +0900

    [Add] LoginModal View, VC 생성 (#96)

---
+++ ACON-iOS/ACON-iOS/Presentation/LoginModal/View/LoginModalView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginModalView.swift
+//  ACON-iOS
+//
+//  Created by 김유림 on 1/22/25.
+//
+
+import UIKit
+
+class LoginModalView: BaseView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
