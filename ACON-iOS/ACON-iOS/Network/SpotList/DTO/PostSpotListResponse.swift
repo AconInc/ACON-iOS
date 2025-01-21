0b2bd8a07dd7d35aa9edc5c8c679bcddac4f33a2
Yurim Kim <yurim830@gmail.com>
2025-01-22 00:49:06 +0900

    [Feat] PostSpotListResponse 작성 (#87)

--- ACON-iOS/ACON-iOS/Network/SpotList/DTO/PostSpotListResponse.swift
+++ ACON-iOS/ACON-iOS/Network/SpotList/DTO/PostSpotListResponse.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct PostSpotListResponse: Codable {
+
+    let spotList: [Spot]
+
+}
+
+struct Spot: Codable {
+    
+    let id: Int
+    let image: String
+    let matchingRate: Int
+    let type: String
+    let name: String
+    let walkingTime: Int
+    
+}
