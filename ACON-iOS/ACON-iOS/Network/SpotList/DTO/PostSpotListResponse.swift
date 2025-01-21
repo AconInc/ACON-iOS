426322d747a23b911693f264eda6583cdc210aad
Yurim Kim <yurim830@gmail.com>
2025-01-22 00:46:39 +0900

    [Add] SpotList Network 관련 파일 생성 및 폴더링 (#87)

---
+++ ACON-iOS/ACON-iOS/Network/SpotList/DTO/PostSpotListResponse.swift
@@ -0,0 +1,8 @@
+//
+//  PostSpotListResponse.swift
+//  ACON-iOS
+//
+//  Created by 김유림 on 1/22/25.
+//
+
+import Foundation
---
+++ ACON-iOS/ACON-iOS/Network/SpotList/SpotListService.swift
@@ -0,0 +1,8 @@
+//
+//  SpotListService.swift
+//  ACON-iOS
+//
+//  Created by 김유림 on 1/21/25.
+//
+
+import Foundation
