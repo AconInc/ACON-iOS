2988154a325beff07a63ecfad8b0f56c536b7b05
cirtuare <cirtuare@gmail.com>
2025-01-22 07:24:43 +0900

    [Chore] dismiss 시 viewWillAppear 호출 문제 관련 Todo 주석 작성 (#71)

--- ACON-iOS/ACON-iOS/Presentation/Upload/View/DropAcornViewController.swift
+++ ACON-iOS/ACON-iOS/Presentation/Upload/View/DropAcornViewController.swift
@@ -50,6 +50,8 @@ class DropAcornViewController: BaseNavViewController {
         super.viewWillAppear(false)
 
         self.tabBarController?.tabBar.isHidden = true
+        // TODO: - 전부 dismiss 될 때 viewWillAppear가 불려짐 - 쓸데없는 API 호출
+        // TODO: - ObservablePattern 사용해서 추후 해결?
         spotReviewViewModel.getAcornCount()
     }
     
