990f0d8177c6ed34744c557c4f603f7c0ecf684f
Yurim Kim <yurim830@gmail.com>
2025-01-22 00:50:26 +0900

    [Chore] SpotListTargetType 작성 (#87)

--- ACON-iOS/ACON-iOS/Network/SpotList/SpotListTargetType.swift
+++ ACON-iOS/ACON-iOS/Network/SpotList/SpotListTargetType.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+import Moya
+
+enum SpotListTargetType {
+
+    case postSpotList(_ requestBody: PostSpotListRequest)
+
+}
+
+extension SpotListTargetType: TargetType {
+
+    var method: Moya.Method {
+        switch self {
+        case .postSpotList:
+            return .post
+        }
+    }
+
+    var path: String {
+        switch self {
+        case .postSpotList:
+            return utilPath + "spots"
+        }
+    }
+
+    var task: Task {
+        switch self {
+        case .postSpotList(let requestBody):
+            return .requestJSONEncodable(requestBody)
+        }
+    }
+
+    var headers: [String : String]? {
+        // TODO: - 추후 유저디폴트 토큰으로 변경
+        let token = ""
+        let headers = HeaderType.headerWithToken(token: "Bearer " + token)
+        return headers
+    }
+
+}
