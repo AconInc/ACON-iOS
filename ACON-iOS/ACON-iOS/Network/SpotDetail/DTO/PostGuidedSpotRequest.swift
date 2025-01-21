efa6a26d9cb3f812ba8062c9e8c4cea9b3d9dd30
cirtuare <cirtuare@gmail.com>
2025-01-21 18:31:51 +0900

    [Feat] PostGuidedSpotRequest 추가 (#85)

---
+++ ACON-iOS/ACON-iOS/Network/SpotDetail/DTO/PostGuidedSpotRequest.swift
@@ -0,0 +1,14 @@
+//
+//  PostGuidedSpotRequest.swift
+//  ACON-iOS
+//
+//  Created by 이수민 on 1/21/25.
+//
+
+import Foundation
+
+struct PostGuidedSpotRequest: Codable {
+    
+    let spotId: Int64
+    
+}
