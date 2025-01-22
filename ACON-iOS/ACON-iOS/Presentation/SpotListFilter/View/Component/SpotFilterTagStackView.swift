525cd932044e632c3f7f1b07dda67b3eb1d8e82f
Yurim Kim <yurim830@gmail.com>
2025-01-23 01:50:13 +0900

    [Chore] 접근제어자 설정 (#87)

--- ACON-iOS/ACON-iOS/Presentation/SpotListFilter/View/Component/SpotFilterTagStackView.swift
+++ ACON-iOS/ACON-iOS/Presentation/SpotListFilter/View/Component/SpotFilterTagStackView.swift
@@ -11,7 +11,7 @@ class SpotFilterTagStackView: UIStackView {
     
     // MARK: - UI Properties
     
-    var tagButtons: [UIButton] = []
+    private var tagButtons: [UIButton] = []
     
     
     // MARK: - LifeCycle
