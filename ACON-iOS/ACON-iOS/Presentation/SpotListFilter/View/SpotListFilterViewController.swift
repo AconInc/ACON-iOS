904a547c517374695ecf6f112b5f562de8a6103e
Yurim Kim <yurim830@gmail.com>
2025-01-23 00:02:00 +0900

    [Feat] conduct filter 액션 설정 (#87)

--- ACON-iOS/ACON-iOS/Presentation/SpotListFilter/View/SpotListFilterViewController.swift
+++ ACON-iOS/ACON-iOS/Presentation/SpotListFilter/View/SpotListFilterViewController.swift
@@ -77,6 +77,12 @@ private extension SpotListFilterViewController {
             action: #selector(didTapExitButton),
             for: .touchUpInside
         )
+        
+        spotListFilterView.conductButton.addTarget(
+            self,
+            action: #selector(didTapConductButton),
+            for: .touchUpInside
+        )
     }
     
 }
@@ -95,6 +101,32 @@ private extension SpotListFilterViewController {
     
     @objc
     func didTapExitButton() {
+        self.dismiss(animated: true)
+    }
+    
+    @objc
+    func didTapConductButton() {
+        switch viewModel.spotType.value {
+        case .restaurant:
+            let restaurantFilter = configureRestaurantFilter()
+            let companionFilter = configureCompanionFilter()
+            
+            viewModel.filterList.append(restaurantFilter)
+            viewModel.filterList.append(companionFilter)
+            
+        case .cafe:
+            let cafeFilter = configureCafeFilter()
+            let visitPurposeFilter = configureVisitPurposeFilter()
+            
+            viewModel.filterList.append(cafeFilter)
+            viewModel.filterList.append(visitPurposeFilter)
+            
+        case .none:
+            print("Impossible Case")
+        }
+        
+        // TODO: 도보 가능 거리, 가격대 필터링
+        viewModel.postSpotList()
         self.dismiss(animated: true)
     }
 }
@@ -128,7 +160,7 @@ private extension SpotListFilterViewController {
 
 // MARK: - Assisting method
 // TODO: 메소드 수정
-//extension SpotListFilterViewController {
+extension SpotListFilterViewController {
 //    
 //    func returnFirstArray<T>(array: T, firstLineCount: Int) -> T where T: RangeReplaceableCollection, T: RandomAccessCollection {
 //        return T(array.prefix(firstLineCount))
@@ -138,4 +170,91 @@ private extension SpotListFilterViewController {
 //        return T(array.dropFirst(firstLineCount))
 //    }
 //
-//}
+    
+    
+    func configureRestaurantFilter() -> SpotFilterListModel {
+        let restaurantFeatures = SpotType.RestaurantFeatureType.allCases
+        var restaurantFeatureOptionList: [String] = []
+        
+        for (i, button) in spotListFilterView.firstLineSpotTagStackView.tagButtons.enumerated() {
+            if button.isSelected {
+                restaurantFeatureOptionList.append(restaurantFeatures[i].serverKey)
+            }
+        }
+        
+        for (i, button) in spotListFilterView.secondLineSpotTagStackView.tagButtons.enumerated() {
+            if button.isSelected {
+                restaurantFeatureOptionList.append(restaurantFeatures[i + 5].serverKey)
+            }
+        }
+        
+        let restaurantFilterList = SpotFilterListModel(
+            category: SpotType.FilterCategoryType.restaurantFeature.serverKey,
+            optionList: restaurantFeatureOptionList
+        )
+        
+        return restaurantFilterList
+    }
+    
+    func configureCafeFilter() -> SpotFilterListModel {
+        let cafeFeatures = SpotType.CafeFeatureType.allCases
+        var cafeFeatureOptionList: [String] = []
+        
+        for (i, button) in spotListFilterView.firstLineSpotTagStackView.tagButtons.enumerated() {
+            if button.isSelected {
+                cafeFeatureOptionList.append(cafeFeatures[i].serverKey)
+            }
+        }
+        
+        for (i, button) in spotListFilterView.secondLineSpotTagStackView.tagButtons.enumerated() {
+            if button.isSelected {
+                cafeFeatureOptionList.append(cafeFeatures[i + 5].serverKey)
+            }
+        }
+        
+        let cafeFilterList = SpotFilterListModel(
+            category: SpotType.FilterCategoryType.cafeFeature.serverKey,
+            optionList: cafeFeatureOptionList
+        )
+        
+        return cafeFilterList
+    }
+    
+    func configureCompanionFilter() -> SpotFilterListModel {
+        let companionType = SpotType.CompanionType.allCases
+        var companionOptionList: [String] = []
+        
+        for (i, button) in spotListFilterView.companionTagStackView.tagButtons.enumerated() {
+            if button.isSelected {
+                companionOptionList.append(companionType[i].serverKey)
+            }
+        }
+        
+        let companionFilterList = SpotFilterListModel(
+            category: SpotType.FilterCategoryType.companion.serverKey,
+            optionList: companionOptionList
+        )
+        
+        return companionFilterList
+    }
+    
+    func configureVisitPurposeFilter() -> SpotFilterListModel {
+        let visitPurpose = SpotType.CompanionType.allCases
+        var visitPurposeOptionList: [String] = []
+        
+        for (i, button) in spotListFilterView.companionTagStackView.tagButtons.enumerated() {
+            if button.isSelected {
+                visitPurposeOptionList.append(visitPurpose[i].serverKey)
+            }
+        }
+        
+        let visitPurposeFilterList = SpotFilterListModel(
+            category: SpotType.FilterCategoryType.companion.serverKey,
+            optionList: visitPurposeOptionList
+        )
+        
+        return visitPurposeFilterList
+    }
+    
+    
+}
