2e5c18c1a9af988511e02930c336c3ec530f6dcb
Yurim Kim <yurim830@gmail.com>
2025-02-08 20:20:08 +0900

    [Chore] ProfileEditView 연결, 탭바 숨김, 네비게이션 설정 (#8)

--- ACON-iOS/ACON-iOS/Global/Literals/StringLiterals.swift
+++ ACON-iOS/ACON-iOS/Global/Literals/StringLiterals.swift
@@ -303,7 +303,9 @@ enum StringLiterals {
     
     enum Profile {
         
-        static let editProfile = "프로필 수정하기"
+        static let profileEditPageTitle = "프로필 편집"
+        
+        static let profileEditButton = "프로필 수정하기"
         
         static let needLogin = "로그인이 필요해요"
         
--- ACON-iOS/ACON-iOS/Presentation/Profile/View/ProfileEditViewController.swift
+++ ACON-iOS/ACON-iOS/Presentation/Profile/View/ProfileEditViewController.swift
@@ -11,12 +11,43 @@ class ProfileEditViewController: BaseNavViewController {
     
     // MARK: - Properties
     
+    private let profileEditView = ProfileEditView()
+    
     
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func setHierarchy() {
+        super.setHierarchy()
+        
+        contentView.addSubview(profileEditView)
+    }
+    
+    override func setLayout() {
+        super.setLayout()
+        
+        profileEditView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    override func setStyle() {
+        super.setStyle()
+        
+        self.setCenterTitleLabelStyle(title: StringLiterals.Profile.profileEditPageTitle)
+        self.setBackButton()
+        
+        profileEditView.setProfileImage(.imgProfileBasic60)
     }
     
 }
