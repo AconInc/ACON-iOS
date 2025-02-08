66349c0478747c896c40cdcd7cf4551c408d7c57
Yurim Kim <yurim830@gmail.com>
2025-02-08 18:43:54 +0900

    [Add] 유효성 메시지 뷰 파일 추가 (#8)

---
+++ ACON-iOS/ACON-iOS/Presentation/Profile/View/Component/ProfileEditValidMessageView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileEditValidMessageView.swift
+//  ACON-iOS
+//
+//  Created by 김유림 on 2/8/25.
+//
+
+import UIKit
+
+class ProfileEditValidMessageView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
