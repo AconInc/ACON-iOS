d23cb5e44ef240f6ada70c662db4742c4db3d603
Yurim Kim <yurim830@gmail.com>
2025-02-08 17:28:48 +0900

    [Add] ProfileModel, ProfileEdit View, VC 생성 (#8)

---
+++ ACON-iOS/ACON-iOS/Presentation/Profile/View/ProfileEditView.swift
@@ -0,0 +1,33 @@
+//
+//  ProfileEditView.swift
+//  ACON-iOS
+//
+//  Created by 김유림 on 2/8/25.
+//
+
+import UIKit
+
+class ProfileEditView: BaseView {
+    
+    // MARK: - UI Properties
+    
+    
+    
+    // MARK: - Life Cycle
+    
+    override func setHierarchy() {
+        super.setHierarchy()
+        
+    }
+
+    override func setLayout() {
+        super.setLayout()
+        
+    }
+    
+    override func setStyle() {
+        super.setStyle()
+        
+    }
+    
+}
---
+++ ACON-iOS/ACON-iOS/Presentation/Profile/View/ProfileEditViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileEditViewController.swift
+//  ACON-iOS
+//
+//  Created by 김유림 on 2/8/25.
+//
+
+import UIKit
+
+class ProfileEditViewController: BaseNavViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
