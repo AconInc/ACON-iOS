f11edefd8e76c89d9dfd45f9cf90d41571727ba8
Yurim Kim <yurim830@gmail.com>
2025-02-08 19:58:20 +0900

    [Feat] ProfileImageEditButton 컴포넌트 구현 (#8)

--- ACON-iOS/ACON-iOS/Presentation/Profile/View/Component/ProfileImageEditButton.swift
+++ ACON-iOS/ACON-iOS/Presentation/Profile/View/Component/ProfileImageEditButton.swift
@@ -8,13 +8,76 @@
 import UIKit
 
 class ProfileImageEditButton: UIButton {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // MARK: - Helpers
+    
+    private let size: CGFloat
+    
+    private let cameraImageSize: Int = 25
+    
+    
+    // MARK: - UI Components
+    
+    private let profileImageView = UIImageView()
+    
+    private let cameraImageView = UIImageView()
+    
+    
+    // MARK: - LifeCycles
+    
+    init(size: CGFloat) {
+        self.size = size
+        
+        super.init(frame: .zero)
+        
+        setStyle()
+        setHierarchy()
+        setLayout()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setStyle() {
+        self.backgroundColor = .gray9
+        
+        profileImageView.do {
+            $0.layer.cornerRadius = size / 2
+            $0.contentMode = .scaleAspectFill
+        }
+        
+        cameraImageView.do {
+            $0.image = .icProfileImgEdit
+            $0.contentMode = .scaleAspectFit
+        }
+    }
+    
+    private func setHierarchy() {
+        self.addSubviews(
+            profileImageView,
+            cameraImageView
+        )
+    }
+    
+    private func setLayout() {
+        self.snp.makeConstraints {
+            $0.size.equalTo(size)
+        }
+        
+        profileImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        cameraImageView.snp.makeConstraints {
+            $0.trailing.bottom.equalTo(profileImageView)
+        }
+    }
+    
+    
+    // MARK: - Internal Methods
+    
+    override func setImage(_ image: UIImage?, for state: UIControl.State = .normal) {
+        profileImageView.image = image
+    }
+    
 }
