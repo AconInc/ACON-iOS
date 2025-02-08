a662fd10a73e796ba2bdb294648768f1d78511e0
Yurim Kim <yurim830@gmail.com>
2025-02-09 05:20:39 +0900

    [Feat] TextField 커스텀 (#8)

--- ACON-iOS/ACON-iOS/Presentation/Profile/View/Component/ProfileEditTextField.swift
+++ ACON-iOS/ACON-iOS/Presentation/Profile/View/Component/ProfileEditTextField.swift
@@ -8,13 +8,96 @@
 import UIKit
 
 class ProfileEditTextField: UITextField {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    // MARK: - UI Settings
+    
+    private let horizontalInset: CGFloat = 12
+    
+    private let clearButton = UIButton()
+    
+    private let clearButtonSize: CGFloat = 28
+    
+    
+    // MARK: - Initializer
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        setStyle()
+        addTarget()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setStyle() {
+        self.do {
+            $0.backgroundColor = .gray9
+            $0.defaultTextAttributes = [
+                .font: ACFontStyleType.s1.font,
+                .kern: ACFontStyleType.s1.kerning,
+                .foregroundColor: UIColor.acWhite
+            ]
+            $0.layer.borderColor = UIColor.gray6.cgColor
+            $0.layer.borderWidth = 1
+            $0.layer.cornerRadius = 4
+            
+            $0.rightView?.frame = CGRect(
+                x: 0,
+                y: 0,
+                width: clearButtonSize + 16 + horizontalInset,
+                height: clearButtonSize
+            )
+            $0.rightView = clearButton
+            $0.rightViewMode = .always
+            
+            $0.leftView = UIView(frame: CGRect(
+                x: 0,
+                y: 0,
+                width: horizontalInset,
+                height: self.frame.height
+            ))
+            $0.leftViewMode = .always
+        }
+        
+        clearButton.do {
+            var config = UIButton.Configuration.plain()
+            config.image = .icDissmissCircleGray
+            config.contentInsets = .init(top: 0, leading: 16, bottom: 0, trailing: horizontalInset)
+            $0.configuration = config
+        }
+    }
+    
+    private func addTarget() {
+        clearButton.addTarget(
+            self,
+            action: #selector(tappedClearButton),
+            for: .touchUpInside
+        )
+    }
+    
+}
 
+
+// MARK: - @objc function
+
+private extension ProfileEditTextField {
+    
+    @objc
+    func tappedClearButton() {
+        self.text = ""
+    }
+    
+}
+
+
+// MARK: - Internal Methods
+
+extension ProfileEditTextField {
+    
+    func setStyle(placeholder: String) {
+        self.attributedPlaceholder = placeholder.ACStyle(.s1, .gray5)
+    }
+    
 }
