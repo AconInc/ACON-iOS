f8bdad1c422e8258a9c5b283fae1c4a6bd848b14
Yurim Kim <yurim830@gmail.com>
2025-02-08 20:44:51 +0900

    [Feat] 유효성메시지View 레이아웃 구현 (#8)

--- ACON-iOS/ACON-iOS/Presentation/Profile/View/Component/ProfileEditValidMessageView.swift
+++ ACON-iOS/ACON-iOS/Presentation/Profile/View/Component/ProfileEditValidMessageView.swift
@@ -7,14 +7,61 @@
 
 import UIKit
 
-class ProfileEditValidMessageView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class ProfileEditValidMessageView: BaseView {
+    
+    // MARK: - UI Properties
+    
+    private let firstIcon = UIImageView()
+    
+    private let firstLine = UILabel()
+    
+    private let secondIcon = UIImageView()
+    
+    private let secondLine = UILabel()
+    
+    
+    // MARK: - Life Cycle
+    
+    override func setHierarchy() {
+        self.addSubviews(
+            firstIcon,
+            firstLine,
+            secondIcon,
+            secondLine
+        )
     }
-    */
-
+    
+    override func setLayout() {
+        firstIcon.snp.makeConstraints {
+            $0.top.leading.equalToSuperview()
+            $0.size.equalTo(20)
+        }
+        
+        firstLine.snp.makeConstraints {
+            $0.leading.equalTo(firstIcon.snp.trailing).offset(4)
+            $0.centerY.equalTo(firstIcon)
+        }
+        
+        secondIcon.snp.makeConstraints {
+            $0.top.equalTo(firstIcon.snp.bottom).offset(4)
+            $0.leading.equalToSuperview()
+            $0.size.equalTo(20)
+        }
+        
+        secondLine.snp.makeConstraints {
+            $0.leading.equalTo(secondIcon.snp.trailing).offset(4)
+            $0.centerY.equalTo(secondIcon)
+        }
+    }
+    
+    override func setStyle() {
+        [firstIcon, secondIcon].forEach {
+            $0.image = .icError20
+        }
+        
+        [firstLine, secondLine].forEach {
+            $0.setLabel(text: "test", style: .s2, color: .red1)
+        }
+    }
+    
 }
