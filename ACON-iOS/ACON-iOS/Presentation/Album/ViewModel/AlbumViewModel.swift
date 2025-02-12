54e645c7839d574abffebff2e953e6154ddea794
Sumin Lee <cirtuare@gmail.com>
2025-02-13 03:14:05 +0900

    [Feat] AlbumVM 사진 불러오는 로직 및 캐싱, 페이지네이션 구현 (#6)

--- ACON-iOS/ACON-iOS/Presentation/Album/ViewModel/AlbumViewModel.swift
+++ ACON-iOS/ACON-iOS/Presentation/Album/ViewModel/AlbumViewModel.swift
@@ -18,10 +18,40 @@ class AlbumViewModel {
     
     let albumSubtypes: [PHAssetCollectionSubtype] = [.smartAlbumUserLibrary, .smartAlbumFavorites, .smartAlbumScreenshots]
     
-
-    func fetchAlbums() {
+    func requestAlbumPermission(completion: @escaping (Bool) -> Void) {
+        PHPhotoLibrary.requestAuthorization(for: .readWrite) { status in
+            DispatchQueue.main.async {
+                switch status {
+                case .authorized, .limited:
+                    print("Album: 권한 허용")
+                    completion(true)
+                case .denied, .restricted, .notDetermined:
+                    print("Album: 권한 거부")
+                    completion(false)
+                @unknown default:
+                    print("Album: 알 수 없는 상태")
+                    completion(false)
+                }
+            }
+        }
+    }
+    
+    // TODO: 사진 권한 관련 alert 적용
+    private func checkPhotoLibraryAuthorization() -> Bool {
         let authStatus = PHPhotoLibrary.authorizationStatus()
-        guard authStatus ==  .authorized || authStatus == .limited else { return }
+        return authStatus == .authorized || authStatus == .limited
+    }
+
+    /// 전체 앨범 가져오기
+    func fetchAlbums() {
+        guard checkPhotoLibraryAuthorization() else {
+            requestAlbumPermission { granted in
+                if granted {
+                    self.fetchAlbums()
+                }
+            }
+            return
+        }
         
         for albumSubtype in albumSubtypes {
             if let smartAlbum = PHAssetCollection.fetchAssetCollections(
@@ -42,15 +72,20 @@ class AlbumViewModel {
                                                                     options: userCreatedAlbumsOptions)
     }
     
+    /// 앨범 전체 사진 가져오기  ( album 인자에 해당하는 앨범 입력, 미입력 시 전체 사진 불러옴)
     func fetchPhotos(in album: PHAssetCollection? = nil) {
-        let authStatus = PHPhotoLibrary.authorizationStatus()
-        guard authStatus ==  .authorized || authStatus == .limited else { return }
+        guard checkPhotoLibraryAuthorization() else {
+            requestAlbumPermission { granted in
+                if granted {
+                    self.fetchAlbums()
+                }
+            }
+            return
+        }
         
         let fetchOptions = PHFetchOptions()
         let sortDescriptor = NSSortDescriptor(key: "creationDate", ascending: false)
-        fetchOptions.fetchLimit = 50
         fetchOptions.sortDescriptors = [sortDescriptor]
-        
         if let album = album {
             photosInCurrentAlbum = PHAsset.fetchAssets(in: album, options: fetchOptions)
         } else {
@@ -58,4 +93,125 @@ class AlbumViewModel {
         }
     }
 
+    // TODO: - 추후 PhotoManager 등으로 리팩
+    private let imageCache = NSCache<NSString, UIImage>()
+    
+    private var imageRequestIDs: [NSString: PHImageRequestID] = [:]
+
+    func setImageCache(for asset: PHAsset,
+                       size: CGSize,
+                       completion: @escaping (UIImage?) -> Void) {
+        let cacheKey = asset.localIdentifier as NSString
+        
+        /// 캐시된 이미지가 있다면 바로 반환
+        if let cachedImage = imageCache.object(forKey: cacheKey) {
+            completion(cachedImage)
+            return
+        }
+        
+        /// 이미지 요청 옵션
+        let options = PHImageRequestOptions()
+        options.deliveryMode = .highQualityFormat
+        options.isNetworkAccessAllowed = true
+        options.resizeMode = .exact
+        options.version = .current
+        
+        /// 이미지 요청
+        let requestID = PHImageManager.default().requestImage(
+            for: asset,
+            targetSize: size,
+            contentMode: .aspectFill,
+            options: options
+        ) { [weak self] image, info in
+            if let image = image {
+                self?.imageCache.setObject(image, forKey: cacheKey)
+                completion(image)
+            } else {
+                // TODO: - 이미지 로딩 실패 시 에러 처리
+                print("image load failed")
+            }
+        }
+        
+        /// 요청 ID 저장
+        imageRequestIDs[cacheKey] = requestID
+    }
+
+    func getCachedImage(for asset: PHAsset) -> UIImage? {
+        let cacheKey = asset.localIdentifier as NSString
+        return imageCache.object(forKey: cacheKey)
+    }
+    
+    func cancelImageLoad(for asset: PHAsset, size: CGSize) {
+        let key = asset.localIdentifier as NSString
+        if let requestID = imageRequestIDs[key] {
+            PHImageManager.default().cancelImageRequest(requestID)
+            imageRequestIDs.removeValue(forKey: key)
+        }
+    }
+    
+    var fetchedImages: [ImageAsset] = []
+    
+    private var currentIndex = 0
+    
+    private let fetchLimit = 80
+
+    private var isLoadingPhotos = false
+    
+    let onSuccessLoadImages: ObservablePattern<Bool> = ObservablePattern(nil)
+    
+    func loadPhotos(in album: PHAssetCollection? = nil,
+                    thumbnailSize: CGSize,
+                    completion: @escaping () -> Void) {
+        guard checkPhotoLibraryAuthorization() else {
+            requestAlbumPermission { granted in
+                if granted {
+                    self.fetchAlbums()
+                }
+            }
+            return
+        }
+
+        guard !isLoadingPhotos else { return }
+        isLoadingPhotos = true
+        
+        // 앨범이 비어있으면 먼저 가져오기
+        if photosInCurrentAlbum.count == 0 {
+            fetchPhotos(in: album)
+        }
+        
+        let endIndex = min(currentIndex + fetchLimit, photosInCurrentAlbum.count)
+        let indexSet = IndexSet(integersIn: currentIndex..<endIndex)
+        
+        let newAssets = indexSet.compactMap { index -> ImageAsset? in
+            guard index < photosInCurrentAlbum.count else { return nil }
+            let asset = photosInCurrentAlbum.object(at: index)
+            return ImageAsset(index: index, asset: asset)
+        }
+        
+        /// 모든 비동기 작업 수행 완료까지 기다리기
+        let group = DispatchGroup()
+        for imageAsset in newAssets {
+            group.enter()
+            setImageCache(for: imageAsset.asset, size: thumbnailSize) { _ in
+                group.leave()
+            }
+        }
+        
+        /// 모든 썸네일 로딩 완료 시 호출
+        group.notify(queue: .main) { [weak self] in
+            guard let self = self else { return }
+            self.fetchedImages.append(contentsOf: newAssets)
+            self.currentIndex += newAssets.count
+            self.isLoadingPhotos = false
+            self.onSuccessLoadImages.value = true
+            completion()
+        }
+        
+        func resetPhotoPagination() {
+            currentIndex = 0
+            fetchedImages.removeAll()
+            isLoadingPhotos = false
+        }
+        
+    }
 }
