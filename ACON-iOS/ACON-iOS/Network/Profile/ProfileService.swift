a9611f78a9ab46737782e28b57504fc7e80cbb30
Yurim Kim <yurim830@gmail.com>
2025-02-15 03:37:32 +0900

    [Add] ProfileService, ProfileTargetType 파일 생성 (#24)

---
+++ ACON-iOS/ACON-iOS/Network/Profile/ProfileService.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileService.swift
+//  ACON-iOS
+//
+//  Created by 김유림 on 2/15/25.
+//
+
+import Foundation
---
+++ ACON-iOS/ACON-iOS/Network/Profile/ProfileTargetType.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileTargetType.swift
+//  ACON-iOS
+//
+//  Created by 김유림 on 2/15/25.
+//
+
+import Foundation
