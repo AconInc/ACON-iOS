f4456bdde3ebc3115e7d66ae4aa462a1dc45b006
Yurim Kim <yurim830@gmail.com>
2025-02-15 04:01:23 +0900

    [Feat] TargetType 작성 (#24)

--- ACON-iOS/ACON-iOS/Network/Profile/ProfileTargetType.swift
+++ ACON-iOS/ACON-iOS/Network/Profile/ProfileTargetType.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+
+import Moya
+
+enum ProfileTargetType {
+    
+    case getProfile
+
+}
+
+extension ProfileTargetType: TargetType {
+
+    var method: Moya.Method {
+        switch self {
+        case .getProfile:
+            return .get
+        }
+    }
+
+    var path: String {
+        switch self {
+        case .getProfile:
+            return utilPath + "members/me"
+        }
+    }
+    
+    var parameter: [String : Any]?  {
+        switch self {
+        default:
+            return .none
+        }
+    }
+
+    var task: Task {
+        switch self {
+        default:
+            return .requestPlain
+        }
+    }
+
+    var headers: [String : String]? {
+        switch self {
+        case .getProfile:
+            return HeaderType.tokenOnly()
+        }
+    }
+
+}
