6c32aacc975684cf5d60d270dfd1d7ae6c5900f6
Sumin Lee <cirtuare@gmail.com>
2025-02-16 06:16:41 +0900

    [Feat] PostReissueResponse & Request (#31)

---
+++ ACON-iOS/ACON-iOS/Network/Auth/DTO/PostReissueResponse.swift
@@ -0,0 +1,16 @@
+//
+//  PostReissueResponse.swift
+//  ACON-iOS
+//
+//  Created by 이수민 on 2/16/25.
+//
+
+import Foundation
+
+struct PostReissueResponse: Codable {
+    
+    let accessToken: String
+    
+    let refreshToken: String
+    
+}
