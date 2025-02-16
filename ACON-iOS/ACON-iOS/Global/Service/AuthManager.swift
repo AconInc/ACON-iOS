c0e1e7d99bb41b20162cc45b0d729894e517c7fd
Yurim Kim <yurim830@gmail.com>
2025-02-16 22:37:23 +0900

    [Chore] 리이슈에 실패했을 경우 모든 UserDefaults 삭제 (#37)

--- ACON-iOS/ACON-iOS/Global/Service/AuthManager.swift
+++ ACON-iOS/ACON-iOS/Global/Service/AuthManager.swift
@@ -51,13 +51,17 @@ extension Serviceable {
                         print("❄️ 기존 서버통신 다시 성공")
                         retryAction()
                     } else {
-                        UserDefaults.standard.removeObject(forKey: StringLiterals.UserDefaults.accessToken)
+                        for key in UserDefaults.standard.dictionaryRepresentation().keys {
+                            UserDefaults.standard.removeObject(forKey: key.description)
+                        }
                         self.navigateToSplash()
                     }
                 }
             } catch {
                 DispatchQueue.main.async {
-                    UserDefaults.standard.removeObject(forKey: StringLiterals.UserDefaults.accessToken)
+                    for key in UserDefaults.standard.dictionaryRepresentation().keys {
+                        UserDefaults.standard.removeObject(forKey: key.description)
+                    }
                     self.navigateToSplash()
                 }
             }
