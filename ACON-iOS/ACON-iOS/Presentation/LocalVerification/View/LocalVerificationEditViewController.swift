3d4e066b10a44abcd5b8fbfefca82dd29be3c4ba
Yurim Kim <yurim830@gmail.com>
2025-02-17 18:32:17 +0900

    [Feat] 동네인증 수정 뷰컨 세팅 (#42)

--- ACON-iOS/ACON-iOS/Presentation/LocalVerification/View/LocalVerificationEditViewController.swift
+++ ACON-iOS/ACON-iOS/Presentation/LocalVerification/View/LocalVerificationEditViewController.swift
@@ -7,23 +7,72 @@
 
 import UIKit
 
-class LocalVerificationEditViewController: BaseViewController {
-
+class LocalVerificationEditViewController: BaseNavViewController {
+    
+    // MARK: - UI Properties
+    
+    private let localVerificationEditView = LocalVerificationEditView()
+    
+    private let localVerificationVM = LocalVerificationViewModel(flowType: .profileEdit)
+    
+    
+    // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        addTarget()
+        bindViewModel()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(false)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        self.tabBarController?.tabBar.isHidden = true
+        
     }
-    */
+    
+    override func setHierarchy() {
+        super.setHierarchy()
+        
+        self.contentView.addSubview(localVerificationEditView)
+    }
+    
+    override func setLayout() {
+        super.setLayout()
 
+        localVerificationEditView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    override func setStyle() {
+        super.setStyle()
+
+        self.setBackButton()
+        self.setSecondTitleLabelStyle(title: StringLiterals.LocalVerification.locateOnMap)
+    }
+    
+    func addTarget() {
+        
+    }
+}
+
+
+// MARK: - Bindings
+
+private extension LocalVerificationEditViewController {
+    
+    func bindViewModel(){
+        localVerificationVM.localAreaName.bind { [weak self] area in
+            guard let self = self,
+                  let area = area else { return }
+            
+//            var newAreas = viewModel.verifiedAreaListEditing.value ?? []
+//            // TODO: VerifiedArea id 수정
+//            newAreas.append(VerifiedAreaModel(id: 1, name: area))
+//            viewModel.verifiedAreaListEditing.value = newAreas
+        }
+    }
+    
 }
