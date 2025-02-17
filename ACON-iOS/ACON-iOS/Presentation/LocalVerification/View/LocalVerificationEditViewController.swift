e286e8d9df3ee6c599c04a04973648f415a2cb97
Yurim Kim <yurim830@gmail.com>
2025-02-17 18:31:01 +0900

    [Add] LocalVerificationEditVC 생성 (#42)

---
+++ ACON-iOS/ACON-iOS/Presentation/LocalVerification/View/LocalVerificationEditViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LocalVerificationEditViewController.swift
+//  ACON-iOS
+//
+//  Created by 김유림 on 2/17/25.
+//
+
+import UIKit
+
+class LocalVerificationEditViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
