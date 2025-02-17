26ce58249e6c088bfe57a6fbbfeb6e7649d643e5
Yurim Kim <yurim830@gmail.com>
2025-02-17 18:03:31 +0900

    [Add] LocalVerificationEditView 생성 (#42)

---
+++ ACON-iOS/ACON-iOS/Presentation/LocalVerification/View/LocalVerificationEditView.swift
@@ -0,0 +1,20 @@
+//
+//  LocalVerificationEditView.swift
+//  ACON-iOS
+//
+//  Created by 김유림 on 2/17/25.
+//
+
+import UIKit
+
+class LocalVerificationEditView: BaseView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
