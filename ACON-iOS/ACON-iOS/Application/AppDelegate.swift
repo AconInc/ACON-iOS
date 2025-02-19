b2dc8f9351b9fa2bc1a119c13122d154aa5d5b6d
Sumin Lee <cirtuare@gmail.com>
2025-02-20 05:12:57 +0900

    [Feat] 화면 세로 고정 (#62)

--- ACON-iOS/ACON-iOS/Application/AppDelegate.swift
+++ ACON-iOS/ACON-iOS/Application/AppDelegate.swift
@@ -29,6 +29,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+    
+    // MARK: - 화면 세로 고정
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .portrait
+    }
 
 }
 
