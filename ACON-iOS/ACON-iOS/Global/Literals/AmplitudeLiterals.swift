3d1b14aaa8579f90943afe552dd57ace0e95685b
cirtuare <cirtuare@gmail.com>
2025-02-23 03:22:07 +0900

    [Feat] AmplitudeLiterals 구현 (#92)

---
+++ ACON-iOS/ACON-iOS/Global/Literals/AmplitudeLiterals.swift
@@ -0,0 +1,32 @@
+//
+//  AmplitudeLiterals.swift
+//  ACON-iOS
+//
+//  Created by 이수민 on 2/23/25.
+//
+
+import Foundation
+
+enum AmplitudeLiterals {
+    
+    enum EventName {
+        
+        static let login = "login"
+        
+        static let areaVerified = "area_verified"
+        
+        static let onboarding = "onboarding"
+        
+        static let mainMenu = "main_menu"
+        
+        static let filter = "filter"
+        
+        static let place_upload = "place_upload"
+        
+        static let service_withdraw = "service_withdra"
+        
+        static let service_logout = "service_logout"
+        
+    }
+    
+}
