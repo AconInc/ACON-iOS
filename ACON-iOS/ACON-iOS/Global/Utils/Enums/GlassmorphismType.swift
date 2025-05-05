f28b94d57434b72c32b3b628087074a30edbc77f
cirtuare <cirtuare@gmail.com>
2025-05-05 20:17:27 +0900

    [Fix] 글라스모피즘 업데이트된 디자인시스템 적용 (#120)

--- ACON-iOS/ACON-iOS/Global/Utils/Enums/GlassmorphismType.swift
+++ ACON-iOS/ACON-iOS/Global/Utils/Enums/GlassmorphismType.swift
@@ -10,33 +10,41 @@ import UIKit
 enum GlassmorphismType {
     
     case buttonGlassDefault, buttonGlassPressed, buttonGlassSelected, buttonGlassDisabled
-    case bottomSheetGlass, actionSheetGlass, alertGlass
+    case bottomSheetGlass, actionSheetGlass, alertGlass, toastGlass, gradientGlass
+    case backgroundGlass
     
     var blurIntensity: CGFloat {
         switch self {
-        case .buttonGlassDefault, .buttonGlassPressed, .buttonGlassSelected, .buttonGlassDisabled:
+        case .buttonGlassDefault, .buttonGlassPressed, .buttonGlassSelected, .buttonGlassDisabled, .toastGlass, .gradientGlass:
             return 0.2
         case .bottomSheetGlass, .alertGlass, .actionSheetGlass:
             return 0.4
+        case .backgroundGlass:
+            return 1
         }
     }
     
     var blurEffectStyle: UIBlurEffect.Style {
         switch self {
-        case .buttonGlassDefault, .buttonGlassPressed, .buttonGlassSelected, .buttonGlassDisabled:
+        case .buttonGlassDefault, .actionSheetGlass, .alertGlass:
+            return .systemThinMaterialLight
+        case .buttonGlassPressed:
             return .systemThickMaterialLight
-        case .bottomSheetGlass, .alertGlass, .actionSheetGlass:
-            return .light
+        case .buttonGlassSelected:
+            return .systemMaterialLight
+        case .buttonGlassDisabled, .bottomSheetGlass:
+            return .systemUltraThinMaterialLight
+        case .toastGlass:
+            return .systemThinMaterialDark
+        case .gradientGlass:
+            return .systemUltraThinMaterialDark
+        case .backgroundGlass:
+            return .systemThinMaterialDark
         }
     }
     
     var vibrancyEffectStyle: UIVibrancyEffectStyle? {
-        switch self {
-        case .buttonGlassDefault, .buttonGlassPressed, .buttonGlassSelected, .buttonGlassDisabled:
-            return nil
-        case .bottomSheetGlass, .alertGlass, .actionSheetGlass:
-            return nil
-        }
+        return nil
     }
     
 }
