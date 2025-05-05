56168874cfd63bec7854db6edd6508f6f1468f69
cirtuare <cirtuare@gmail.com>
2025-05-05 12:27:47 +0900

    [Feat] Glassmorphism Border 구현 (#120)

--- ACON-iOS/ACON-iOS/Global/UIComponents/ACButton.swift
+++ ACON-iOS/ACON-iOS/Global/UIComponents/ACButton.swift
@@ -7,11 +7,37 @@
 
 import UIKit
 
+struct GlassBorderAttributes {
+    
+    let width: CGFloat
+    
+    let cornerRadius: CGFloat
+    
+    let glassmorphismType: GlassmorphismType
+    
+    init(width: CGFloat, cornerRadius: CGFloat, glassmorphismType: GlassmorphismType) {
+        self.width = width
+        self.cornerRadius = cornerRadius
+        self.glassmorphismType = glassmorphismType
+    }
+    
+}
+
 class ACButton: UIButton {
     
     // MARK: - UI Properties
     
     private var glassmorphismView: GlassmorphismView?
+    
+    private var borderLayer: CAShapeLayer?
+    
+    
+    // MARK: - Properties
+    
+    private var glassBorderAttributes: GlassBorderAttributes?
+
+    
+    // MARK: - Lifecycle
     
     init(
         style: ButtonStyleType,
@@ -34,7 +60,11 @@ class ACButton: UIButton {
         glassmorphismView?.layer.cornerRadius = self.layer.cornerRadius
         glassmorphismView?.layer.masksToBounds = true
         
+        if let attributes = glassBorderAttributes, bounds.width > 0, bounds.height > 0 {
+            applyGlassBorder(attributes)
+        }
     }
+    
 }
 
 extension ACButton {
@@ -87,10 +117,15 @@ extension ACButton {
         self.do {
             $0.backgroundColor = style.backgroundColor
             
-            if let blurIntensity = style.blurIntensity,
-               let blurEffectStyle = style.blurEffectStyle {
-                setBlurEffect(blurIntensity, blurEffectStyle)
+            if let glassmorphismType = style.glassmorphismType {
+                /// borderWidth > 0 이면 테두리만 글모, 0일 경우 전체 배경 글모
+                if style.borderWidth > 0 && style.borderColor == .clear {
+                    glassBorderAttributes = GlassBorderAttributes(width: style.borderWidth,
+                                                                  cornerRadius: style.cornerRadius,
+                                                                  glassmorphismType: glassmorphismType)
+                } else {
                     setGlassmorphism(glassmorphismType)
+                }
             }
             
             $0.layer.cornerRadius = style.cornerRadius
@@ -126,6 +161,37 @@ extension ACButton {
 
         glassmorphismView?.snp.makeConstraints {
             $0.edges.equalToSuperview()
+        }
+    }
+    
+    
+    // MARK: - Set Glassmorphism Border
+
+    private func applyGlassBorder(_ attributes: GlassBorderAttributes) {
+        glassmorphismView?.removeFromSuperview()
+        borderLayer?.removeFromSuperlayer()
+        self.layer.borderWidth = 0
+        
+        let outerPath = UIBezierPath(roundedRect: bounds, cornerRadius: attributes.cornerRadius)
+        let innerRect = bounds.insetBy(dx: attributes.width, dy: attributes.width)
+        let innerPath = UIBezierPath(roundedRect: innerRect, cornerRadius: max(0, attributes.cornerRadius - attributes.width/2))
+        outerPath.append(innerPath.reversing())
+        
+        glassmorphismView = GlassmorphismView(attributes.glassmorphismType)
+        
+        if let glassmorphismView = glassmorphismView {
+            self.insertSubview(glassmorphismView, at: 0)
+            glassmorphismView.snp.makeConstraints {
+                $0.edges.equalToSuperview()
+            }
+            
+            let maskLayer = CAShapeLayer()
+            maskLayer.path = outerPath.cgPath
+            maskLayer.fillRule = .evenOdd
+            
+            let maskView = UIView(frame: bounds)
+            maskView.layer.addSublayer(maskLayer)
+            glassmorphismView.mask = maskView
         }
     }
     
