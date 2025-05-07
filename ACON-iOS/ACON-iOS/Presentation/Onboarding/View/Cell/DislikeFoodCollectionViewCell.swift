e7f93c95c8ff90fd0a313c69ad4c73f4095aa8cf
Sumin Lee <cirtuare@gmail.com>
2025-05-08 06:45:52 +0900

    [Feat] DislikeFoodCollectionViewCell 구현 (#118)

---
+++ ACON-iOS/ACON-iOS/Presentation/Onboarding/View/Cell/DislikeFoodCollectionViewCell.swift
@@ -0,0 +1,68 @@
+//
+//  DislikeFoodCollectionViewCell.swift
+//  ACON-iOS
+//
+//  Created by 이수민 on 5/8/25.
+//
+
+import UIKit
+
+final class DislikeFoodCollectionViewCell: BaseCollectionViewCell {
+
+    // MARK: - UI Properties
+    
+    private let chipButton : ACButton = ACButton(style: GlassButton(glassmorphismType: .buttonGlassDefault, buttonType: .full_100_b1r))
+    
+    
+    // MARK: - Lifecycle
+    
+    override func setHierarchy() {
+        super.setHierarchy()
+        
+        self.addSubview(chipButton)
+    }
+    
+    override func setLayout() {
+        super.setLayout()
+        
+        chipButton.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    override func setStyle() {
+        super.setStyle()
+        
+        self.backgroundColor = .clear
+    }
+    
+    override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
+        let width = chipButton.intrinsicContentSize.width + 24
+        
+        var newFrame = layoutAttributes.frame
+        newFrame.size.width = width
+        newFrame.size.height = 38
+        layoutAttributes.frame = newFrame
+        
+        return layoutAttributes
+    }
+
+    //TODO: - ACButton 글모 디자인 제대로 안 뜨는 문제 !!!!!!!!!
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        chipButton.frame = self.bounds
+    }
+    
+}
+
+extension DislikeFoodCollectionViewCell {
+    
+    func bindData(_ title: String?, _ indexRow: Int) {
+        chipButton.updateGlassButtonTitle(title ?? "")
+        
+        setNeedsLayout()
+        layoutIfNeeded()
+    }
+    
+}
