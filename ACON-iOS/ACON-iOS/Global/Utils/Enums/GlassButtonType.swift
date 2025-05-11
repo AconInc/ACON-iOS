08f6983318f25328d2cb45dd9eae1529fd53a3a0
Yurim Kim <yurim830@gmail.com>
2025-05-11 21:29:55 +0900

    [Feat] GlassConfigButton 생성, GlassButtonType에 contentInsets 추가 (#129)

--- ACON-iOS/ACON-iOS/Global/Utils/Enums/GlassButtonType.swift
+++ ACON-iOS/ACON-iOS/Global/Utils/Enums/GlassButtonType.swift
@@ -36,6 +36,23 @@ enum GlassButtonType {
         }
     }
     
+    var contentInsets: NSDirectionalEdgeInsets {
+        switch self {
+        case .full_12_t4SB:
+            return .init(top: 15, leading: 15, bottom: 15, trailing: 15)
+        case .line_12_b1SB: // TODO: 무슨 컴포넌트인지 못찾겠어서 임의 수치 넣음. 추후 수정
+            return .init(top: 15, leading: 15, bottom: 15, trailing: 15)
+        case .full_22_b1SB:
+            return .init(top: 12, leading: 12, bottom: 12, trailing: 12)
+        case .full_19_b1R:
+            return .init(top: 9, leading: 12, bottom: 9, trailing: 12)
+        case .full_10_b1SB:
+            return .init(top: 8, leading: 8, bottom: 8, trailing: 8)
+        case .both_20_labelAction_t4R:
+            return .init(top: 12, leading: 12, bottom: 12, trailing: 12)
+        }
+    }
+    
     var textStyle: ACFontType {
         switch self {
         case .full_22_b1SB, .line_12_b1SB, .full_10_b1SB:
