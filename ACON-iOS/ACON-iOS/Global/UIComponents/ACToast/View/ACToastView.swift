e330d5a6d70a12ad1211a07d6daf64e9e4731dc4
cirtuare <cirtuare@gmail.com>
2025-05-23 12:24:23 +0900

    [Feat] ACToastController 재정립 (#149)

---
+++ ACON-iOS/ACON-iOS/Global/UIComponents/ACToast/View/ACToastView.swift
@@ -0,0 +1,90 @@
+//
+//  ACToastView.swift
+//  ACON-iOS
+//
+//  Created by 이수민 on 5/23/25.
+//
+
+import UIKit
+
+final class ACToastView: GlassmorphismView {
+    
+    //MARK: - UI Properties
+    
+    private let messageLabel: UILabel = UILabel()
+    
+    private let tapAction: (() -> Void)?
+    
+    private var acToastType: ACToastType
+    
+    
+    // MARK: - LifeCycle
+    
+    init(_ acToastType: ACToastType, _ tapAction: (() -> Void)?) {
+        self.acToastType = acToastType
+        self.tapAction = tapAction
+        
+        super.init(.toastGlass)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func setHierarchy() {
+        super.setHierarchy()
+        
+        addSubviews(messageLabel)
+    }
+    
+    override func setLayout() {
+        super.setLayout()
+        
+        self.snp.makeConstraints {
+            $0.height.equalTo(ScreenUtils.heightRatio*56)
+            $0.width.equalTo(ScreenUtils.widthRatio*328)
+        }
+        
+        messageLabel.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(ScreenUtils.widthRatio*18)
+            $0.centerY.equalToSuperview()
+        }
+    }
+    
+    override func setStyle() {
+        super.setStyle()
+        
+        messageLabel.setLabel(text: acToastType.title,
+                              style: acToastType.titleFont,
+                              color: acToastType.titleColor,
+                              alignment: .center)
+        
+        addTapAction()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        self.addGlassBorder(acToastType.glassBorderAttributes)
+    }
+
+}
+
+
+// MARK: - Tap Action
+
+private extension ACToastView {
+    
+    @objc
+    func handleTapAction() {
+        tapAction?()
+    }
+    
+    func addTapAction() {
+        let tapGesture = UITapGestureRecognizer(target: self,
+                                                action: #selector(handleTapAction))
+        self.isUserInteractionEnabled = true
+        self.addGestureRecognizer(tapGesture)
+    }
+    
+}
