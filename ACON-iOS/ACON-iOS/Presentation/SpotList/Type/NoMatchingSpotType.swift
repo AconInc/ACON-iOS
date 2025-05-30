33755bc85063c6977442c1eabcd2d9feb6b436e5
Yurim Kim <yurim830@gmail.com>
2025-05-30 18:52:40 +0900

    [Add] enum 추가 - noMatchingItemSize, noMatching, transportMode (#156)

---
+++ ACON-iOS/ACON-iOS/Presentation/SpotList/Type/NoMatchingSpotType.swift
@@ -0,0 +1,15 @@
+//
+//  NoMatchingSpotType.swift
+//  ACON-iOS
+//
+//  Created by 김유림 on 5/29/25.
+//
+
+import Foundation
+
+enum NoMatchingSpotType {
+
+    case withSuggestion
+    case noSuggestion
+
+}
