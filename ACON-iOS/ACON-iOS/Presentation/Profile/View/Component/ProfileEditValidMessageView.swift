eceb08504553c58d2a694c3cee29db778efe721b
Yurim Kim <yurim830@gmail.com>
2025-06-06 00:15:17 +0900

    [Chore] 유효성메시지 컴포넌트 2.0 수정 (#164)

--- ACON-iOS/ACON-iOS/Presentation/Profile/View/Component/ProfileEditValidMessageView.swift
+++ ACON-iOS/ACON-iOS/Presentation/Profile/View/Component/ProfileEditValidMessageView.swift
@@ -11,62 +11,35 @@ final class ProfileEditValidMessageView: BaseView {
 
     // MARK: - UI Properties
 
-    private let firstIcon = UIImageView()
+    private let icon = UIImageView()
 
-    private let firstLine = UILabel()
-
-    private let secondIcon = UIImageView()
-
-    private let secondLine = UILabel()
+    private let label = UILabel()
 
 
     // MARK: - UI Setting Methods
 
     override func setHierarchy() {
-        self.addSubviews(
-            firstIcon,
-            firstLine,
-            secondIcon,
-            secondLine
-        )
+        self.addSubviews(icon, label)
     }
 
     override func setLayout() {
-        firstIcon.snp.makeConstraints {
+        icon.snp.makeConstraints {
             $0.top.leading.equalToSuperview()
-            $0.size.equalTo(20)
+            $0.size.equalTo(18)
         }
 
-        firstLine.snp.makeConstraints {
-            $0.leading.equalTo(firstIcon.snp.trailing).offset(4)
-            $0.centerY.equalTo(firstIcon)
-        }
-
-        secondIcon.snp.makeConstraints {
-            $0.top.equalTo(firstIcon.snp.bottom).offset(4)
-            $0.bottom.leading.equalToSuperview()
-            $0.size.equalTo(20)
-        }
-
-        secondLine.snp.makeConstraints {
-            $0.leading.equalTo(secondIcon.snp.trailing).offset(4)
-            $0.centerY.equalTo(secondIcon)
+        label.snp.makeConstraints {
+            $0.leading.equalTo(icon.snp.trailing).offset(4)
+            $0.centerY.equalTo(icon)
         }
     }
 
     override func setStyle() {
-        hideFirstLine(true)
-        hideSecondLine(true)
+        self.isHidden = true
     }
 
     private func hideFirstLine(_ isHidden: Bool) {
-        [firstIcon, firstLine].forEach {
-            $0.isHidden = isHidden
-        }
-    }
-
-    private func hideSecondLine(_ isHidden: Bool) {
-        [secondIcon, secondLine].forEach {
+        [icon, label].forEach {
             $0.isHidden = isHidden
         }
     }
@@ -75,35 +48,19 @@ final class ProfileEditValidMessageView: BaseView {
     // MARK: - Internal Methods
 
     func setValidMessage(_ type: ProfileValidMessageType) {
-        let idxErrMsg: String = "⚠️Index out of range"
-
         switch type {
         case .none:
-            hideFirstLine(true)
-            hideSecondLine(true)
+            self.isHidden = true
 
-        case .nicknameMissing, .nicknameTaken, .invalidDate, .areaMissing, .invalidSymbol, .invalidLanguage:
-            guard type.texts.count == 1 else { print(idxErrMsg); return }
-            hideFirstLine(false)
-            hideSecondLine(true)
-            firstIcon.image = .icError
-            firstLine.setLabel(text: type.texts[0], style: .s2, color: .labelDanger)
+        case .nicknameMissing, .nicknameTaken, .invalidDate, .invalidChar:
+            self.isHidden = false
+            icon.image = .icExclamationMark
+            label.setLabel(text: type.text, style: .b1R, color: .labelDanger)
 
         case .nicknameOK:
-            guard type.texts.count == 1 else { print(idxErrMsg); return }
-            hideFirstLine(false)
-            hideSecondLine(true)
-            firstIcon.image = .icLocalCheckMark
-            firstLine.setLabel(text: type.texts[0], style: .s2, color: .labelAction)
-
-        case .invalidSymbolAndLang:
-            guard type.texts.count == 2 else { print(idxErrMsg); return }
-            hideFirstLine(false)
-            hideSecondLine(false)
-            firstIcon.image = .icError
-            firstLine.setLabel(text: type.texts[0], style: .s2, color: .labelDanger)
-            secondIcon.image = .icError
-            secondLine.setLabel(text: type.texts[1], style: .s2, color: .labelDanger)
+            self.isHidden = false
+            icon.image = .icSuccess
+            label.setLabel(text: type.text, style: .b1R, color: .labelSuccess)
         }
     }
 
