d88b1ee7e485309bf235a4d3b9c7e7f7efbc51a5
cirtuare <cirtuare@gmail.com>
2025-06-07 04:21:50 +0900

    [Feat] ProfileGoogleAdView 구현 (#162)

--- ACON-iOS/ACON-iOS/Global/Utils/Enums/GlassButtonType.swift
+++ ACON-iOS/ACON-iOS/Global/Utils/Enums/GlassButtonType.swift
@@ -21,6 +21,8 @@ enum GlassButtonType {
     
     case full_19_b1R
     
+    case full_14_b1R
+    
     case both_20_labelAction_t4R
     
     var cornerRadius: CGFloat {
@@ -31,6 +33,8 @@ enum GlassButtonType {
             return 19
         case .both_20_labelAction_t4R:
             return 20
+        case .full_14_b1R:
+            return 14
         case .full_12_t4SB, .line_12_b1SB:
             return 12
         case .full_10_b1SB:
@@ -44,7 +48,7 @@ enum GlassButtonType {
             return .init(top: 15, leading: 15, bottom: 15, trailing: 15)
         case .line_12_b1SB, .full_22_b1SB, .line_22_b1SB:
             return .init(top: 12, leading: 12, bottom: 12, trailing: 12)
-        case .full_19_b1R:
+        case .full_19_b1R, .full_14_b1R:
             return .init(top: 9, leading: 12, bottom: 9, trailing: 12)
         case .full_10_b1SB:
             return .init(top: 8, leading: 8, bottom: 8, trailing: 8)
@@ -61,7 +65,7 @@ enum GlassButtonType {
             return .t4R
         case .full_12_t4SB:
             return .t4SB
-        case .full_19_b1R:
+        case .full_19_b1R, .full_14_b1R:
             return .b1R
         }
     }
