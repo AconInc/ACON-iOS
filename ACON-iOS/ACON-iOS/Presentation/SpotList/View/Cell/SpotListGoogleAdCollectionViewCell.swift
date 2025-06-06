c749425195b129196e14b6280470cefe6c207e96
cirtuare <cirtuare@gmail.com>
2025-06-07 04:19:21 +0900

    [Feat] SpotListGoogleAdCollectionViewCell 구현 (#162)

---
+++ ACON-iOS/ACON-iOS/Presentation/SpotList/View/Cell/SpotListGoogleAdCollectionViewCell.swift
@@ -0,0 +1,241 @@
+//
+//  NativeAdCell.swift
+//  ACON-iOS
+//
+//  Created by 이수민 on 6/4/25.
+//
+
+import UIKit
+
+import GoogleMobileAds
+
+class SpotListGoogleAdCollectionViewCell: BaseCollectionViewCell {
+    
+    // MARK: - UI Properties
+    
+    private let glassmorphismView: GlassmorphismView = GlassmorphismView(.buttonGlassDefault)
+    
+    private let nativeAdView = NativeAdView()
+    
+    private let adButton = ACButton(style: GlassButton(glassmorphismType: .buttonGlassDefault, buttonType: .full_19_b1R), title: "광고")
+    
+    private let headlineLabel = UILabel()
+    
+    private let bodyLabel = UILabel()
+    
+    private let iconImageView = UIImageView()
+    
+    private let mediaView = MediaView()
+
+    private let callToActionButton = ACButton(style: GlassButton(glassmorphismType: .buttonGlassDefault, buttonType: .full_10_b1SB))
+    
+    private let skeletonView = SkeletonView()
+    
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setNativeAdView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func setHierarchy() {
+        contentView.addSubviews(glassmorphismView, nativeAdView)
+
+        nativeAdView.addSubviews(mediaView,
+                                 adButton,
+                                 headlineLabel,
+                                 bodyLabel,
+                                 iconImageView,
+                                 callToActionButton)
+    }
+    
+    override func setLayout() {
+        super.setLayout()
+        
+        let edge = ScreenUtils.widthRatio * 20
+        
+        glassmorphismView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        nativeAdView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+//        
+//        skeletonView.snp.makeConstraints {
+//            $0.edges.equalToSuperview()
+//        }
+        
+        adButton.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(52*ScreenUtils.heightRatio)
+            $0.leading.equalToSuperview().inset(edge)
+            $0.width.equalTo(48)
+            $0.height.equalTo(38)
+        }
+        
+        iconImageView.snp.makeConstraints {
+            $0.leading.bottom.equalToSuperview().inset(edge)
+            $0.size.equalTo(36)
+        }
+        
+        headlineLabel.snp.makeConstraints {
+            $0.top.leading.equalTo(edge)
+            $0.width.equalTo(210)
+            $0.height.equalTo(24)
+        }
+        
+        bodyLabel.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(76*ScreenUtils.heightRatio)
+            $0.horizontalEdges.equalToSuperview().inset(edge)
+            $0.height.equalTo(40)
+        }
+        
+        mediaView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        callToActionButton.snp.makeConstraints {
+            $0.bottom.trailing.equalToSuperview().inset(edge)
+            $0.width.equalTo(140)
+            $0.height.equalTo(36)
+        }
+    }
+    
+    override func setStyle() {
+        contentView.do {
+            $0.layer.cornerRadius = 12
+            $0.layer.shadowOffset = CGSize(width: 0, height: 2)
+            $0.layer.shadowRadius = 4
+            $0.layer.shadowOpacity = 0.1
+            $0.backgroundColor = .clear
+        }
+        
+        skeletonView.isHidden = true
+        
+        iconImageView.do {
+            $0.contentMode = .scaleAspectFill
+            $0.layer.cornerRadius = 8
+            $0.clipsToBounds = true
+            $0.backgroundColor = .systemGray6
+        }
+        
+        mediaView.do {
+            $0.layer.cornerRadius = 8
+            $0.clipsToBounds = true
+        }
+
+        [adButton, callToActionButton].forEach {
+            $0.do {
+                $0.updateGlassButtonState(state: .default)
+                $0.isUserInteractionEnabled = false
+            }
+        }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        nativeAdView.nativeAd = nil
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        glassmorphismView.layer.cornerRadius = 12
+        glassmorphismView.clipsToBounds = true
+        
+        glassmorphismView.refreshBlurEffect()
+        adButton.updateGlassButtonState(state: .default)
+        callToActionButton.updateGlassButtonState(state: .default)
+    }
+}
+
+
+// MARK: - Configure
+
+extension SpotListGoogleAdCollectionViewCell {
+    
+    private func setNativeAdView() {
+        nativeAdView.do {
+            $0.headlineView = headlineLabel
+            $0.bodyView = bodyLabel
+            $0.callToActionView = callToActionButton
+            $0.iconView = iconImageView
+            $0.mediaView = mediaView
+        }
+    }
+    
+    func configure(with nativeAd: NativeAd) {
+        nativeAd.delegate = self
+        
+        nativeAdView.isHidden = false
+        skeletonView.isHidden = true
+        
+        if let headline = nativeAd.headline {
+            headlineLabel.setLabel(text: headline, style: .t4SB)
+            headlineLabel.isHidden = false
+        } else {
+            headlineLabel.isHidden = true
+        }
+        
+        if let body = nativeAd.body {
+            bodyLabel.setLabel(text: body, style: .b1R)
+            bodyLabel.isHidden = false
+        } else {
+            bodyLabel.isHidden = true
+        }
+        
+        if let callToAction = nativeAd.callToAction {
+            callToActionButton.updateButtonTitle(callToAction)
+        } else {
+            callToActionButton.updateButtonTitle("자세히 보기")
+        }
+        
+        if let icon = nativeAd.icon {
+            iconImageView.image = icon.image
+            iconImageView.isHidden = false
+        } else {
+            iconImageView.isHidden = true
+        }
+        
+        let mediaContent = nativeAd.mediaContent
+        mediaView.mediaContent = mediaContent
+        
+        nativeAdView.nativeAd = nativeAd
+    }
+    
+}
+
+
+// MARK: - Empty Cell
+
+extension SpotListGoogleAdCollectionViewCell {
+    
+    func showSkeleton() {
+        nativeAdView.isHidden = true
+        // TODO: - 스켈레톤 애니메이션 적용
+    }
+    
+}
+
+
+// MARK: - NativeAdDelegate
+
+extension SpotListGoogleAdCollectionViewCell: NativeAdDelegate {
+    
+    // TODO: - 광고 관련 기록 -> 추후 엠플 사용?
+    func nativeAdDidRecordClick(_ nativeAd: NativeAd) {
+        print("Spotlist 광고 클릭됨")
+    }
+    
+    func nativeAdDidRecordImpression(_ nativeAd: NativeAd) {
+        print("Spotlist 광고 인식됨")
+    }
+    
+}
