eccb83f7b78b84205101826aa5dd41f93b960950
cirtuare <cirtuare@gmail.com>
2025-06-18 22:13:21 +0900

    [Fix] ProfileSettingVC 스크롤 막기 (#207)

--- ACON-iOS/ACON-iOS/Presentation/Profile/View/ProfileSettingViewController.swift
+++ ACON-iOS/ACON-iOS/Presentation/Profile/View/ProfileSettingViewController.swift
@@ -67,6 +67,7 @@ final class ProfileSettingViewController: BaseNavViewController {
             $0.sectionHeaderTopPadding = 0
             $0.separatorInset = UIEdgeInsets(top: 16, left: 0, bottom: 0, right: 0)
             $0.separatorStyle = .none
+            $0.isScrollEnabled = false
         }
     }
 
