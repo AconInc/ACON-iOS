8df52fb5a1ce5cf5e00e2d784ffc7b1d272deeb3
Yurim Kim <yurim830@gmail.com>
2025-08-05 15:15:50 +0900

    [Chore] 리뷰 작성 시 위치 인증 API 수정 (#242)

--- ACON-iOS/ACON-iOS/Network/Upload/UploadTargetType.swift
+++ ACON-iOS/ACON-iOS/Network/Upload/UploadTargetType.swift
@@ -39,7 +39,7 @@ extension UploadTargetType: TargetType {
         case .getSearchKeyword:
             return utilPath + "spots/search"
         case .getReviewVerification:
-            return utilPath + "spots/verify"
+            return utilPath + "reviews/verify"
         case .getAcornCount:
             return utilPath + "members/acorn"
         }
