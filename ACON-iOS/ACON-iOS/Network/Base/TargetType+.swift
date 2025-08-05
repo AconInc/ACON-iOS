b1e8428869c13b9512871b667c116104ef2c13b8
Yurim Kim <yurim830@gmail.com>
2025-08-05 14:41:14 +0900

    [Add,Feat] ApiVersionType 구현 및 utilPath 리팩토링 (#242)

--- ACON-iOS/ACON-iOS/Network/Base/TargetType+.swift
+++ ACON-iOS/ACON-iOS/Network/Base/TargetType+.swift
@@ -10,7 +10,7 @@ import Foundation
 import Moya
 
 extension TargetType {
-    
+
     var baseURL: URL {
         guard let urlString = Bundle.main.object(forInfoDictionaryKey: Config.Keys.Plist.baseURL) as? String,
               let url = URL(string: urlString) else {
@@ -18,9 +18,14 @@ extension TargetType {
         }
         return url
     }
-    
-    var utilPath: String {
-        return "api/v1/"
+
+    /// NOTE: 기본 API 버전 (필요 시 개별 Target에서 오버라이드)
+    var apiVersion: ApiVersionType {
+        return .v1
     }
-    
+
+    var utilPath: String {
+        return apiVersion.utilPath
+    }
+
 }
