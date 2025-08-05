41fafd9cee9fe8f9d23171078182888be40c2d45
Yurim Kim <yurim830@gmail.com>
2025-08-05 21:49:41 +0900

    [Feat] postSpot 메소드 구현 (#251)

--- ACON-iOS/ACON-iOS/Presentation/SpotUpload/ViewModel/SpotUploadViewModel.swift
+++ ACON-iOS/ACON-iOS/Presentation/SpotUpload/ViewModel/SpotUploadViewModel.swift
@@ -29,4 +29,72 @@ final class SpotUploadViewModel: Serviceable {
     
     var isWorkFriendly: Bool? = nil
 
+
+    // MARK: - Network
+
+    func postSpot() {
+        let request = PostSpotUploadRequest(
+            spotName: selectedSpot?.spotName ?? "",
+            address: selectedSpot?.spotAddress ?? "",
+            spotType: spotType?.serverKey ?? "",
+            featureList: configureFeatureList(),
+            recommendedMenu: recommendedMenu ?? "",
+            imageList: presignedURLs.isEmpty ? nil : presignedURLs
+        )
+
+        ACService.shared.spotUploadService.postSpotUpload(requestBody: request) { [weak self] response in
+            switch response {
+            case .success(_):
+                self?.onSuccessPostSpot.value = true
+            case .reIssueJWT:
+                self?.handleReissue { [weak self] in
+                    self?.postSpot()
+                }
+            default:
+                self?.handleNetworkError { [weak self] in
+                    self?.postSpot()
+                }
+            }
+        }
+    }
+}
+
+
+// MARK: - Helper
+
+private extension SpotUploadViewModel {
+
+    func configureFeatureList() -> [SpotUploadFeatureDTO] {
+        guard let spotType else { return [] }
+
+        var featureListDTO: [SpotUploadFeatureDTO] = []
+
+        // NOTE: 식당 or 카페 feature
+        switch spotType {
+        case .restaurant:
+            let restaurantFeature = SpotUploadFeatureDTO(
+                category: SpotUploadType.restaurantFeature.serverKey,
+                optionList: restaurantFeature.map { $0.serverKey }
+            )
+            featureListDTO.append(restaurantFeature)
+        case .cafe:
+            if (isWorkFriendly ?? false) {
+                let cafeFeature = SpotUploadFeatureDTO(
+                    category: SpotUploadType.cafeFeature.serverKey,
+                    optionList: [SpotUploadType.CafeOptionType.workFriendly.serverKey]
+                )
+                featureListDTO.append(cafeFeature)
+            }
+        }
+
+        // NOTE: 가성비
+        let priceFeature = SpotUploadFeatureDTO(
+            category: SpotUploadType.price.serverKey,
+            optionList: [priceValue?.serverKey ?? ""]
+        )
+        featureListDTO.append(priceFeature)
+
+        return featureListDTO
+    }
+
 }
