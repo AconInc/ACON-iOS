bb079c2d509eda1cf84f9e3cd35c53a5ab840f50
Yurim Kim <yurim830@gmail.com>
2025-08-21 18:35:17 +0900

    [Feat] 튜토리얼 컨테이너 PageVC, PageControl 구현 (#261)

--- ACON-iOS/ACON-iOS/Presentation/Onboarding/View/Tutorial/TutorialContainerViewController.swift
+++ ACON-iOS/ACON-iOS/Presentation/Onboarding/View/Tutorial/TutorialContainerViewController.swift
@@ -9,4 +9,129 @@ import UIKit
 
 class TutorialContainerViewController: BaseViewController {
 
+    // MARK: - Properties
+
+    lazy var pages: [UIViewController] = [localReviewVC, onlyFiftySpotsVC, startNowVC]
+
+    private var currentIndex: Int = 0
+
+
+    // MARK: - UI Properties
+
+    private var pageVC = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
+    private let pageControl = UIPageControl()
+
+    // NOTE: tutorial pages
+    lazy var localReviewVC = LocalReviewViewController()
+    lazy var onlyFiftySpotsVC = OnlyFiftySpotsViewController()
+    lazy var startNowVC = StartNowViewController()
+
+
+    // MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setDelegate()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(false)
+
+        self.tabBarController?.tabBar.isHidden = true
+    }
+
+
+    // MARK: - UI Setting
+
+    override func setHierarchy() {
+        super.setHierarchy()
+
+        self.addChild(pageVC)
+
+        view.addSubviews(pageVC.view, pageControl)
+
+        pageVC.didMove(toParent: self)
+    }
+
+    override func setLayout() {
+        super.setLayout()
+
+        pageVC.view.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+
+        pageControl.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(24 * ScreenUtils.heightRatio)
+            $0.centerX.equalToSuperview()
+        }
+    }
+
+    override func setStyle() {
+        super.setStyle()
+
+        pageVC.setViewControllers([pages[0]], direction: .forward, animated: false, completion: nil)
+
+        pageControl.do {
+            $0.numberOfPages = pages.count
+            $0.currentPage = 0
+            $0.pageIndicatorTintColor = .gray300
+            $0.currentPageIndicatorTintColor = .acWhite
+            $0.isUserInteractionEnabled = false
+            $0.transform = CGAffineTransform(scaleX: 1, y: 1)
+        }
+        
+    }
+
+    private func setDelegate() {
+        pageVC.dataSource = self
+        pageVC.delegate = self
+    }
+
+}
+
+
+// MARK: - DataSource
+
+extension TutorialContainerViewController: UIPageViewControllerDataSource {
+
+    // NOTE: 이전 페이지
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let currentIndex = pages.firstIndex(of: viewController) else { return nil }
+        guard currentIndex > 0 else { return nil }
+        return pages[currentIndex - 1]
+    }
+
+    // NOTE: 다음 페이지
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let currentIndex = pages.firstIndex(of: viewController) else { return nil }
+        guard currentIndex < pages.count - 1 else { return nil }
+        return pages[currentIndex + 1]
+    }
+
+}
+
+
+// MARK: - UIPageViewControllerDelegate
+
+extension TutorialContainerViewController: UIPageViewControllerDelegate {
+
+    // NOTE: transition 시작될 때 pageControl 이동
+    func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
+        guard let upcomingVC = pendingViewControllers.first,
+              let upcomingIndex = pages.firstIndex(of: upcomingVC) else { return }
+
+        pageControl.currentPage = upcomingIndex
+    }
+
+    // NOTE: transition 취소됐을 때 pageControl 이동
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if !completed {
+            guard let currentVC = pageViewController.viewControllers?.first,
+                  let currentIndex = pages.firstIndex(of: currentVC) else { return }
+
+            pageControl.currentPage = currentIndex
+        }
+    }
+
 }
