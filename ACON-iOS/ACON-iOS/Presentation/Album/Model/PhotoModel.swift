03573f934454388161671f3fd59eb16d1bd22b4b
Yurim Kim <yurim830@gmail.com>
2025-09-22 01:29:58 +0900

    [Add] PhotoModel 생성 (#272)

---
+++ ACON-iOS/ACON-iOS/Presentation/Album/Model/PhotoModel.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoModel.swift
+//  ACON-iOS
+//
+//  Created by 김유림 on 9/22/25.
+//
+
+import UIKit
+import Photos
+
+struct PhotoModel: Equatable {
+
+    let asset: PHAsset
+
+    let image: UIImage
+
+}
