ab3a3941ef5c39e300ca1e1eb8250a5bfbb614cb
Yurim Kim <yurim830@gmail.com>
2025-09-22 22:44:29 +0900

    [Refactor] patchProfile을 async throws로 변경 (#272)

--- ACON-iOS/ACON-iOS/Presentation/Profile/ViewModel/ProfileViewModel.swift
+++ ACON-iOS/ACON-iOS/Presentation/Profile/ViewModel/ProfileViewModel.swift
@@ -146,16 +146,17 @@ final class ProfileViewModel: Serviceable {
 
         Task {
             do {
+                var imageURL: String? = nil
                 if let profileImage {
-                    let imageURL = try await uploadProfilePhoto(asset: profileImage.asset)
-                    patchProfile(imageURL: imageURL)
-                } else {
-                    patchProfile()
+                    imageURL = try await uploadProfilePhoto(asset: profileImage.asset)
                 }
+                try await patchProfile(imageURL: imageURL)
+                await MainActor.run { onPatchProfileSuccess.value = true }
+
             } catch PhotoManagerError.tokenExpired {
                 guard self.uploadRetryCount < self.maxUploadRetries else {
                     print("🚨 Max retries reached. Stopping the loop.")
-                    onPatchProfileSuccess.value = false
+                    await MainActor.run { onPatchProfileSuccess.value = false }
                     return
                 }
                 self.uploadRetryCount += 1
@@ -168,30 +169,31 @@ final class ProfileViewModel: Serviceable {
                 }
             } catch {
                 print("❌ An unhandled failure occurred: \(error.localizedDescription)")
-                onPatchProfileSuccess.value = false
+                await MainActor.run { onPatchProfileSuccess.value = false }
             }
         }
     }
 
-    func patchProfile(imageURL: String? = nil) {
+    func patchProfile(imageURL: String? = nil) async throws {
         let requestBody = PatchProfileRequest(
             profileImage: imageURL,
             nickname: userInfo.nickname,
             birthDate: userInfo.birthDate
         )
 
-        ACService.shared.profileService.patchProfile(requestBody: requestBody) { [weak self] response in
-            guard let self = self else { return }
-            switch response {
-            case .success:
-                onPatchProfileSuccess.value = true
-            case .reIssueJWT:
-                self.handleReissue {
-                    self.patchProfile(imageURL: imageURL)
-                }
-            default:
-                self.handleNetworkError {
-                    self.patchProfile(imageURL: imageURL)
+        try await withCheckedThrowingContinuation { continuation in
+            ACService.shared.profileService.patchProfile(requestBody: requestBody) { response in
+                switch response {
+                case .success:
+                    continuation.resume(returning: ())
+                case .reIssueJWT:
+                    continuation.resume(throwing: PhotoManagerError.tokenExpired)
+                case .requestErr(let error):
+                    continuation.resume(throwing: PhotoManagerError.requestError(error))
+                case .networkFail:
+                    continuation.resume(throwing: PhotoManagerError.networkError)
+                default:
+                    continuation.resume(throwing: PhotoManagerError.serverError)
                 }
             }
         }
